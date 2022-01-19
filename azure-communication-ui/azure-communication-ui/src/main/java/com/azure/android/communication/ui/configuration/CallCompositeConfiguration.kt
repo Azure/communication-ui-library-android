6051cdee30367285ae5102beed9146ce89440e10
adamhammer <adamhammer@microsoft.com>
2022-01-19 14:52:50 -0800

    apply formatting

--- azure-communication-ui/azure-communication-ui/src/main/java/com/azure/android/communication/ui/configuration/CallCompositeConfiguration.kt
+++ azure-communication-ui/azure-communication-ui/src/main/java/com/azure/android/communication/ui/configuration/CallCompositeConfiguration.kt
@@ -37,11 +37,13 @@ internal class CallCompositeConfiguration {
 
         // / Gets a config by it's ID
         // / May return null if the Configuration becomes garbage collected
-        fun getConfig(id: Int): CallCompositeConfiguration = configs[id]?.get() ?:
-        throw RuntimeException( if (configs.containsKey(id))
-            "Config with ID: $id was likely disposed, this is possible if the launching Activity was destroyed." +
-                     "Consider scoping your CallComposite class to an Application instead of Activity."
-        else
-            "This ID is not valid, and no entry exists in the map. Please file a bug, this is an error in the composite")
+        fun getConfig(id: Int): CallCompositeConfiguration = configs[id]?.get()
+            ?: throw RuntimeException(
+            if (configs.containsKey(id))
+                "Config with ID: $id was likely disposed, this is possible if the launching Activity was destroyed." +
+                    "Consider scoping your CallComposite class to an Application instead of Activity."
+            else
+                "This ID is not valid, and no entry exists in the map. Please file a bug, this is an error in the composite"
+        )
     }
 }
