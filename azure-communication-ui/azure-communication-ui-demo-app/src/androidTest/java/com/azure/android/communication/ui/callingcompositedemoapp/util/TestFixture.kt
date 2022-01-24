73b2bd035fb438ab2bb6033a5b892d4c372e93a3
Albert Lo <v-albertlo@microsoft.com>
2022-01-24 12:48:30 -0800

    ktLint fixes

--- azure-communication-ui/azure-communication-ui-demo-app/src/androidTest/java/com/azure/android/communication/ui/callingcompositedemoapp/util/TestFixture.kt
+++ azure-communication-ui/azure-communication-ui-demo-app/src/androidTest/java/com/azure/android/communication/ui/callingcompositedemoapp/util/TestFixture.kt
@@ -1,7 +1,8 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 package com.azure.android.communication.ui.callingcompositedemoapp.util
 
 import androidx.test.platform.app.InstrumentationRegistry
-
 
 object TestFixture {
     val teamsUrl by lazy {
