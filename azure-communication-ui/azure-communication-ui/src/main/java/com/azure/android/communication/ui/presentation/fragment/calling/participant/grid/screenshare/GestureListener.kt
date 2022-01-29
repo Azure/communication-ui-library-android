4ababdf9c7c92faffbc6ab77c936450d8a242f66
Inderpal Singh Aulakh <73618019+iaulakh@users.noreply.github.com>
2022-01-28 19:50:34 -0800

    Using valid pointer for transformation

--- azure-communication-ui/azure-communication-ui/src/main/java/com/azure/android/communication/ui/presentation/fragment/calling/participant/grid/screenshare/GestureListener.kt
+++ azure-communication-ui/azure-communication-ui/src/main/java/com/azure/android/communication/ui/presentation/fragment/calling/participant/grid/screenshare/GestureListener.kt
@@ -9,6 +9,10 @@ import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.ScaleGestureDetector
 
+/**
+ * The GestureListener detects click as well as scale and notify zoom frame layout to apply transformation
+ * Used some parts of https://github.com/facebook/fresco zoomable to implement pinch and zoom
+ */
 internal class GestureListener(
     context: Context,
     private val gestureListenerEvents: GestureListenerEvents,
@@ -16,11 +20,20 @@ internal class GestureListener(
     GestureDetector.OnGestureListener,
     GestureDetector.OnDoubleTapListener {
 
+    companion object {
+        private const val INVALID_POINTER = MotionEvent.INVALID_POINTER_ID
+    }
+
+    init {
+        reset()
+    }
+
     private var pointerStartPosition = PointF(0f, 0f)
     private var pointerCurrentPosition = PointF(0f, 0f)
     private var scaleFactor = 1.0f
     private var scaleGestureDetector = ScaleGestureDetector(context, this)
     private val clickGestureDetector = GestureDetector(context, this)
+    private var validPointerIndex = INVALID_POINTER
 
     val scale get() = scaleFactor
     val startX get() = pointerStartPosition.x
@@ -30,30 +43,26 @@ internal class GestureListener(
 
     fun onTouchEvent(event: MotionEvent): Boolean {
         clickGestureDetector.onTouchEvent(event)
+        scaleGestureDetector.onTouchEvent(event)
         when (event.actionMasked) {
             MotionEvent.ACTION_MOVE -> {
-                pointerCurrentPosition.x = event.getX(0)
-                pointerCurrentPosition.y = event.getY(0)
-                gestureListenerEvents.updateTransformation()
+                updateGesture(event)
             }
             MotionEvent.ACTION_DOWN,
             MotionEvent.ACTION_POINTER_DOWN,
             MotionEvent.ACTION_POINTER_UP,
             MotionEvent.ACTION_UP,
             -> {
-                pointerStartPosition.x = event.getX(0)
-                pointerStartPosition.y = event.getY(0)
-                gestureListenerEvents.initTransformation()
+                numberOfPointersChanged(event)
             }
         }
-        scaleGestureDetector.onTouchEvent(event)
         return true
     }
 
     fun resetPointers() {
         pointerStartPosition.x = pointerCurrentPosition.x
         pointerStartPosition.y = pointerCurrentPosition.y
-        gestureListenerEvents.initTransformation()
+        startGesture()
     }
 
     override fun onScale(scaleDetector: ScaleGestureDetector): Boolean {
@@ -110,5 +119,58 @@ internal class GestureListener(
     override fun onDoubleTapEvent(e: MotionEvent?): Boolean {
         e?.let { gestureListenerEvents.onDoubleClick(it) }
         return true
+    }
+
+    private fun reset() {
+        validPointerIndex = INVALID_POINTER
+    }
+
+    private fun updateGesture(event: MotionEvent) {
+        updatePointersOnMove(event)
+        if (validPointerIndex != INVALID_POINTER) {
+            startGesture()
+        }
+        gestureListenerEvents.updateTransformation()
+    }
+
+    private fun numberOfPointersChanged(event: MotionEvent) {
+        updatePointersOnTap(event)
+        if (validPointerIndex != INVALID_POINTER) {
+            startGesture()
+        }
+    }
+
+    private fun startGesture() {
+        gestureListenerEvents.initTransformation()
+    }
+
+    private fun getPressedPointerIndex(event: MotionEvent): Int {
+        val count = event.pointerCount
+        val action = event.actionMasked
+        val index = event.actionIndex
+        var temp = 0
+        if ((action == MotionEvent.ACTION_UP || action == MotionEvent.ACTION_POINTER_UP) && temp >= index) {
+            temp++
+        }
+        return if (temp < count) temp else INVALID_POINTER
+    }
+
+    private fun updatePointersOnTap(event: MotionEvent) {
+        val index = getPressedPointerIndex(event)
+        if (index != INVALID_POINTER) {
+            validPointerIndex = event.getPointerId(index)
+            pointerStartPosition.x = event.getX(index)
+            pointerStartPosition.y = event.getY(index)
+        } else {
+            validPointerIndex = INVALID_POINTER
+        }
+    }
+
+    private fun updatePointersOnMove(event: MotionEvent) {
+        val index = event.findPointerIndex(validPointerIndex)
+        if (index != INVALID_POINTER) {
+            pointerCurrentPosition.x = event.getX(index)
+            pointerCurrentPosition.y = event.getY(index)
+        }
     }
 }
