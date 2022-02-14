36d8af3fe5f51ce7f7d36b2e73fec59505115999
Albert Lo <OdysseyDesigns.ltd@gmail.com>
2022-02-14 15:38:42 -0800

    [Feature] Teams Url UI test cases (#52)

--- azure-communication-ui/azure-communication-ui-demo-app/src/androidTest/java/com/azure/android/communication/ui/callingcompositedemoapp/CallingCompositeBaselineUiTest.kt
+++ azure-communication-ui/azure-communication-ui-demo-app/src/androidTest/java/com/azure/android/communication/ui/callingcompositedemoapp/CallingCompositeBaselineUiTest.kt
@@ -3,57 +3,21 @@
 
 package com.azure.android.communication.ui.callingcompositedemoapp
 
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import androidx.test.rule.GrantPermissionRule
-import com.azure.android.communication.ui.callingcompositedemoapp.util.CompositeUiHelper
-import com.azure.android.communication.ui.callingcompositedemoapp.util.NetworkUtils
+import com.azure.android.communication.ui.callingcompositedemoapp.robots.HomeScreenRobot
 import com.azure.android.communication.ui.callingcompositedemoapp.util.RunWhenScreenOffOrLockedRule
 import com.azure.android.communication.ui.callingcompositedemoapp.util.TestFixture
-import com.microsoft.appcenter.espresso.Factory
-import com.microsoft.appcenter.espresso.ReportHelper
-import org.junit.After
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
-class CallingCompositeBaselineUiTest {
-
-    @get:Rule
-    var reportHelper: ReportHelper = Factory.getReportHelper()
-
-    @Rule
-    @JvmField
-    var activityTestRule = ActivityScenarioRule(CallLauncherActivity::class.java)
-    @Rule
-    @JvmField
-    var grantPermissionRule: GrantPermissionRule =
-        GrantPermissionRule.grant(
-            "android.permission.FOREGROUND_SERVICE",
-            "android.permission.ACCESS_NETWORK_STATE",
-            "android.permission.WAKE_LOCK",
-            "android.permission.MODIFY_AUDIO_SETTINGS",
-            "android.permission.CAMERA",
-            "android.permission.RECORD_AUDIO"
-        )
+class CallingCompositeBaselineUiTest : BaseUiTest() {
 
     @get:Rule
     val screenLockRule = RunWhenScreenOffOrLockedRule()
-
-    @Before
-    fun ciToolSetup() {
-        Thread.sleep(2000)
-        reportHelper.label("Starting test")
-    }
-
-    @After
-    fun tearDown() {
-        reportHelper.label("Stopping test")
-    }
 
     @Test
     fun testJoinTeamsCallWithVideoEnabled() {
@@ -75,59 +39,38 @@ class CallingCompositeBaselineUiTest {
         joinGroupCall()
     }
 
-    @Test
-    fun testJoinTeamsCallAfterNetworkDisconnected() {
-        joinAfterNetworkDisconnected(false)
-    }
-
-    @Test
-    fun testJoinGroupCallAfterNetworkDisconnected() {
-        joinAfterNetworkDisconnected()
-    }
-
-    private fun joinAfterNetworkDisconnected(isGroupCall: Boolean = true) {
-        NetworkUtils.disableNetwork()
-        CompositeUiHelper.run {
-            if (isGroupCall) {
-                setGroupIdOrTeamsMeetingUrl(TestFixture.groupId)
-            } else {
-                clickTeamsMeetingRadioButton()
-                setGroupIdOrTeamsMeetingUrl(TestFixture.teamsUrl)
-            }
-
-            startAndJoinCall(TestFixture.acsToken, true)
-            dismissNetworkLossSnackbar()
-
-            NetworkUtils.enableNetworkThatWasDisabled {
-                navigateUpFromSetupScreen()
-                clickAlertDialogOkButton()
-            }
-        }
-    }
-
     private fun joinTeamsCall(videoEnabled: Boolean = true) {
-        CompositeUiHelper.run {
-            clickTeamsMeetingRadioButton()
-            setGroupIdOrTeamsMeetingUrl(TestFixture.teamsUrl)
-            startAndJoinCall(TestFixture.acsToken, videoEnabled)
+        val setupScreen = HomeScreenRobot()
+            .clickTeamsMeetingRadioButton()
+            .setGroupIdOrTeamsMeetingUrl(TestFixture.teamsUrl)
+            .setAcsToken(TestFixture.acsToken)
+            .clickLaunchButton()
 
-            checkWaitForTeamsMeetingMessage()
-            clickEndCall()
-            clickLeaveCall()
+        if (videoEnabled) {
+            setupScreen.turnCameraOn()
         }
+        val callScreen = setupScreen.clickJoinCallButton()
+        callScreen
+            .checkWaitForTeamsMeetingMessage()
+            .clickEndCall()
+            .clickLeaveCall()
     }
 
     private fun joinGroupCall(videoEnabled: Boolean = true) {
-        CompositeUiHelper.run {
-            setGroupIdOrTeamsMeetingUrl(TestFixture.groupId)
+        val setupScreen = HomeScreenRobot()
+            .setGroupIdOrTeamsMeetingUrl(TestFixture.groupId)
+            .setAcsToken(TestFixture.acsToken)
+            .clickLaunchButton()
 
-            startAndJoinCall(TestFixture.acsToken, videoEnabled)
-            showParticipantList()
-            checkParticipantList()
-
-            dismissParticipantList()
-            clickEndCall()
-            clickLeaveCall()
+        if (videoEnabled) {
+            setupScreen.turnCameraOn()
         }
+        val callScreen = setupScreen.clickJoinCallButton()
+        callScreen
+            .showParticipantList()
+            .checkParticipantList()
+            .dismissParticipantList()
+            .clickEndCall()
+            .clickLeaveCall()
     }
 }
---
+++ azure-communication-ui/azure-communication-ui-demo-app/src/androidTest/java/com/azure/android/communication/ui/callingcompositedemoapp/CallingCompositeNetworkTest.kt
@@ -0,0 +1,72 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+package com.azure.android.communication.ui.callingcompositedemoapp
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.azure.android.communication.ui.callingcompositedemoapp.robots.HomeScreenRobot
+import com.azure.android.communication.ui.callingcompositedemoapp.util.NetworkUtils
+import com.azure.android.communication.ui.callingcompositedemoapp.util.TestFixture
+import com.azure.android.communication.ui.callingcompositedemoapp.util.ViewIsDisplayedResource
+import org.junit.AfterClass
+import org.junit.BeforeClass
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class CallingCompositeNetworkTest : BaseUiTest() {
+    companion object {
+        @BeforeClass
+        @JvmStatic
+        fun networkTestSetup() {
+            ViewIsDisplayedResource.TIMED_OUT_VALUE = 60000L
+        }
+
+        @AfterClass
+        @JvmStatic
+        fun networkTestTeardown() {
+            ViewIsDisplayedResource.TIMED_OUT_VALUE = 30000L
+        }
+    }
+
+    @Test
+    fun testJoinTeamsCallAfterNetworkDisconnected() {
+        NetworkUtils.disableNetwork()
+        val homeScreen = HomeScreenRobot()
+            .setAcsToken(TestFixture.acsToken)
+            .clickTeamsMeetingRadioButton()
+            .setGroupIdOrTeamsMeetingUrl(TestFixture.teamsUrl)
+        joinAfterNetworkDisconnected(homeScreen)
+    }
+
+    @Test
+    fun testJoinGroupCallAfterNetworkDisconnected() {
+        NetworkUtils.disableNetwork()
+        val homeScreen = HomeScreenRobot()
+            .setAcsToken(TestFixture.acsToken)
+            .setGroupIdOrTeamsMeetingUrl(TestFixture.groupId)
+
+        joinAfterNetworkDisconnected(homeScreen)
+    }
+
+    private fun joinAfterNetworkDisconnected(homeScreen: HomeScreenRobot) {
+
+        val setupScreen = homeScreen.clickLaunchButton()
+        try {
+            val callScreen = setupScreen
+                .turnCameraOn()
+                .clickJoinCallButton()
+
+            setupScreen.dismissNetworkLossBanner()
+        } catch (ex: Throwable) {
+            println("Runtime Error: " + ex.message)
+            throw ex
+        } finally {
+            NetworkUtils.enableNetworkThatWasDisabled {
+                setupScreen.navigateUpFromSetupScreen()
+                homeScreen.clickAlertDialogOkButton()
+            }
+        }
+    }
+}
