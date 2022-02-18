e9c8776b6cdf7286b17ddc2f3b02425115bd9d40
adamhammer <adamhammer@microsoft.com>
2022-02-18 15:23:20 -0800

    Add tests for Audio Selection actions

--- azure-communication-ui/azure-communication-ui/src/test/java/com/azure/android/communication/ui/redux/reducer/LocalParticipantReduxStateReducerUnitTest.kt
+++ azure-communication-ui/azure-communication-ui/src/test/java/com/azure/android/communication/ui/redux/reducer/LocalParticipantReduxStateReducerUnitTest.kt
@@ -530,4 +530,49 @@ internal class LocalParticipantReduxStateReducerUnitTest {
         // assert
         Assert.assertEquals(AudioOperationalStatus.ON, newState.audioState.operation)
     }
+
+    // / Helper for Audio Device Selection/Requested tests
+    fun deviceStateReducer_reduce_when_AudioDeviceRequested(audioDeviceSelectionStatus: AudioDeviceSelectionStatus) {
+
+        // arrange
+        val reducer = LocalParticipantStateReducerImpl()
+        val oldState = LocalUserState(
+            CameraState(
+                CameraOperationalStatus.PENDING,
+                CameraDeviceSelectionStatus.FRONT,
+                CameraTransmissionStatus.LOCAL
+            ),
+            AudioState(AudioOperationalStatus.ON, audioDeviceSelectionStatus),
+            videoStreamID = null,
+            displayName = null
+        )
+
+        val action =
+            LocalParticipantAction.AudioDeviceChangeRequested(audioDeviceSelectionStatus)
+
+        // act
+        val newState = reducer.reduce(oldState, action)
+
+        // assert
+        Assert.assertEquals(
+            audioDeviceSelectionStatus,
+            newState.audioState.device
+        )
+    }
+
+    @Test
+    fun deviceStateReducer_reduce_when_AudioDeviceRequested_request_receiver() =
+        deviceStateReducer_reduce_when_AudioDeviceRequested(AudioDeviceSelectionStatus.RECEIVER_REQUESTED)
+
+    @Test
+    fun deviceStateReducer_reduce_when_AudioDeviceRequested_request_bluetooth() =
+        deviceStateReducer_reduce_when_AudioDeviceRequested(AudioDeviceSelectionStatus.BLUETOOTH_SCO_REQUESTED)
+
+    @Test
+    fun deviceStateReducer_reduce_when_AudioDeviceRequested_select_receiver() =
+        deviceStateReducer_reduce_when_AudioDeviceRequested(AudioDeviceSelectionStatus.RECEIVER_SELECTED)
+
+    @Test
+    fun deviceStateReducer_reduce_when_AudioDeviceRequested_select_bluetooth() =
+        deviceStateReducer_reduce_when_AudioDeviceRequested(AudioDeviceSelectionStatus.BLUETOOTH_SCO_SELECTED)
 }
