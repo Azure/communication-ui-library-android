817d1d066f59093d2dcce48e05bf53cb433bc64f
ShaunaSong <93549644+ShaunaSong@users.noreply.github.com>
2022-02-22 18:54:06 -0400

    Update locale API

--- azure-communication-ui/azure-communication-ui/src/main/java/com/azure/android/communication/ui/configuration/LocalizationConfiguration.kt
+++ azure-communication-ui/azure-communication-ui/src/main/java/com/azure/android/communication/ui/configuration/LocalizationConfiguration.kt
@@ -1,18 +1,18 @@
 package com.azure.android.communication.ui.configuration
 
 class LocalizationConfiguration {
-    var language: String? = ""
-    var isRTL: Boolean? = false
+    var language: String = ""
+    var isRTL: Boolean = false
 
     constructor (
-        language: String?,
-        isRTL: Boolean?,
+        language: String,
+        isRTL: Boolean
     ) {
         this.language = language
         this.isRTL = isRTL
     }
 
-    constructor(language: String?) {
+    constructor(language: String) {
         this.language = language
     }
 }
