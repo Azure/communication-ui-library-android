1fcb4edc2d67c5e5b1094074d79ea35bcb198292
Mohtasim <mohtasim@avijit.redmond.corp.microsoft.com>
2022-02-22 16:29:40 -0800

    [Android][CallingComposite] Adding an a list of supported language as an enum class. [Task-ID] 2772729
    Signed-off-by: Mohtasim <mohtasim@avijit.redmond.corp.microsoft.com>

---
+++ azure-communication-ui/azure-communication-ui/src/main/java/com/azure/android/communication/ui/utilities/SupportedLanguages.kt
@@ -0,0 +1,17 @@
+package com.azure.android.communication.ui.utilities
+
+enum class SupportedLanguages(val languageCode: String) {
+    CHINESE_SIMPLIFIED("zh-CN"),
+    SPANISH("es"),
+    RUSSIAN("ru"),
+    JAPANESE("ja"),
+    FRENCH("fr"),
+    BRAZILIAN_PORTUGUESE("pt-BR"),
+    GERMAN("de"),
+    KOREAN("ko"),
+    ITALIAN("it"),
+    CHINESE_TRADITIONAL("zh-TW"),
+    DUTCH("nl"),
+    TURKISH("tr"),
+    ENGLISH_UK("en-GB");
+}
