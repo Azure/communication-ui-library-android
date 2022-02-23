a8aea37e78668f0c0a99ba76a6bc041029fb7b24
Mohtasim <mohtasim@avijit.redmond.corp.microsoft.com>
2022-02-23 09:24:01 -0800

    [Android][UI] Making the language selector persistent [Task-ID] 2772729
    Signed-off-by: Mohtasim <mohtasim@avijit.redmond.corp.microsoft.com>

--- azure-communication-ui/azure-communication-ui-demo-app/src/main/java/com/azure/android/communication/ui/callingcompositedemoapp/SettingsActivity.kt
+++ azure-communication-ui/azure-communication-ui-demo-app/src/main/java/com/azure/android/communication/ui/callingcompositedemoapp/SettingsActivity.kt
@@ -1,7 +1,11 @@
 package com.azure.android.communication.ui.callingcompositedemoapp
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Toast
@@ -9,24 +13,39 @@ import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.LinearLayoutCompat
 import com.azure.android.communication.ui.CallComposite
 import com.azure.android.communication.ui.callingcompositedemoapp.launcher.FeatureFlagView
+import com.azure.android.communication.ui.utilities.FEATURE_FLAG_SHARED_PREFS_KEY
 import com.azure.android.communication.ui.utilities.SupportedLanguages
 
 class SettingsActivity : AppCompatActivity() {
 
-    val supportedLanguages = SupportedLanguages.values()
-    lateinit var autoCompleteTextView: AutoCompleteTextView
+    private val supportedLanguages = SupportedLanguages.values()
+    private lateinit var autoCompleteTextView: AutoCompleteTextView
+    private lateinit var languageArrayAdapter: ArrayAdapter<SupportedLanguages>
+    private val sharedPreference by lazy {
+        getSharedPreferences(FEATURE_FLAG_SHARED_PREFS_KEY, Context.MODE_PRIVATE)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
         autoCompleteTextView = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)
-        val languageArrayAdapter = ArrayAdapter(applicationContext, R.layout.language_dropdown_item, supportedLanguages)
+        languageArrayAdapter = ArrayAdapter(applicationContext, R.layout.language_dropdown_item, supportedLanguages)
         autoCompleteTextView.setAdapter(languageArrayAdapter)
-
-        Toast.makeText(this,"This is settings page", Toast.LENGTH_SHORT).show()
+        languageArrayAdapter.filter.filter(null)
     }
 
+    override fun onResume() {
+        super.onResume()
+
+        autoCompleteTextView.setText(sharedPreference.getString("LANGUAGE_ADAPTER_POSITION", "ENGLISH_UK"), true)
+        languageArrayAdapter.filter.filter(null)
+
+        autoCompleteTextView.setOnItemClickListener{ parent, view, position, id->
+            val selectedItem: String = autoCompleteTextView.adapter.getItem(position).toString()
+            sharedPreference.edit().putString("LANGUAGE_ADAPTER_POSITION", selectedItem).commit()
+        }
+    }
 
 
 }
