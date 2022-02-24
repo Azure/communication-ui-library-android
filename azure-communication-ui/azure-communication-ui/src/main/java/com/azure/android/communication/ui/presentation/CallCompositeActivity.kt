06729e0a01eb9586c43fdf10371f03ee3f9b151c
Inderpal Singh Aulakh <73618019+iaulakh@users.noreply.github.com>
2022-02-25 02:01:48 +0530

    [Bug] fragment launched twice

--- azure-communication-ui/azure-communication-ui/src/main/java/com/azure/android/communication/ui/presentation/CallCompositeActivity.kt
+++ azure-communication-ui/azure-communication-ui/src/main/java/com/azure/android/communication/ui/presentation/CallCompositeActivity.kt
@@ -28,6 +28,7 @@ import com.azure.android.communication.ui.presentation.fragment.setup.SetupFragm
 import com.azure.android.communication.ui.presentation.navigation.BackNavigation
 import com.azure.android.communication.ui.redux.action.CallingAction
 import com.azure.android.communication.ui.redux.state.NavigationStatus
+import com.microsoft.fluentui.util.activity
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
@@ -197,6 +198,15 @@ internal class CallCompositeActivity : AppCompatActivity() {
     }
 
     private fun launchFragment(fragmentClassName: String) {
+        activity?.supportFragmentManager?.fragments?.let {
+            if (it.isNotEmpty()) {
+                // during call rotate below logic helps to avoid launching fragment twice
+                if (it.last().javaClass.name.equals(fragmentClassName)) {
+                    return
+                }
+            }
+        }
+
         val fragment = supportFragmentManager.fragmentFactory.instantiate(
             classLoader,
             fragmentClassName
