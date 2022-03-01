2e0c85c3f97d5c3a151e6bba289624745442d62e
Inderpal Singh Aulakh <73618019+iaulakh@users.noreply.github.com>
2022-03-01 20:42:14 +0530

    Update RemoteParticipantManagerImpl.kt

--- azure-communication-ui/azure-communication-ui/src/main/java/com/azure/android/communication/ui/participant/remote/RemoteParticipantManagerImpl.kt
+++ azure-communication-ui/azure-communication-ui/src/main/java/com/azure/android/communication/ui/participant/remote/RemoteParticipantManagerImpl.kt
@@ -2,12 +2,18 @@ package com.azure.android.communication.ui.participant.remote
 
 import com.azure.android.communication.common.CommunicationIdentifier
 import com.azure.android.communication.ui.AvatarPersonaData
+import kotlinx.coroutines.flow.MutableStateFlow
 
 internal class RemoteParticipantManagerImpl: RemoteParticipantManager {
+
+    val avatarPersonaDataStateFlow: MutableStateFlow<AvatarPersonaData?> = MutableStateFlow(null)
+
+
     override fun setRemoteParticipantAvatar(
         communicationIdentifier: CommunicationIdentifier,
         avatarPersonaData: AvatarPersonaData,
     ) {
+        avatarPersonaDataStateFlow.value = avatarPersonaData
     }
 
     override fun getRemoteParticipantAvatar(communicationIdentifier: CommunicationIdentifier): AvatarPersonaData {
