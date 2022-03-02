a4efd6354548ad93cdb4415d09cb5bfa9d26c52e
adamhammer <adamhammer@microsoft.com>
2022-03-01 16:48:24 -0800

    initial conversion to java

---
+++ azure-communication-ui/azure-communication-ui/src/main/java/com/azure/android/communication/ui/utilities/FeatureFlags.java
@@ -0,0 +1,229 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.android.communication.ui.utilities;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import com.azure.android.communication.ui.R;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Consumer;
+
+/* Feature Flag Management
+
+There is 2 parts to this system.
+1) Enum entries (fixed features). These are features in the Composite Library itself
+2) Pluggable entries (dynamic features). This allows the Application (e.g. Demo, Contoso) to add
+   additional features using the system.
+
+Initialization:
+  The feature flag system requires an application context to access String and Boolean resources
+  as well as the SharedPreferences to retain choices
+
+  `FeatureFlags.initialize(context)` anywhere early in your code e.g. (activity/application).onCreate
+
+# Usage:
+  ## Checking Flags:
+  ### Built-in
+  `FeatureFlags.FlagName.active = true/false`
+  (enable/disable) a feature or check it's status
+
+  ### Add-on
+  `yourFeatureFlag.active = true/false`
+  (enable/disable) a feature or check it's status
+
+  ## Registering a feature from outside UI Composite Library
+  `FeatureFlags.registerAdditionalFeature(yourFeatureFlag)`
+
+  ## Reading all FeatureFlags (Enum + Addon)
+  `FeatureFlags.features`
+  This is recommended over "values()" which will only access the Enum values.
+
+ */
+
+enum FeatureFlags implements FeatureFlag {
+
+    // ---------------------------- Global Features -------------------------------------------------
+    // These features are global to the composite. They are available via the FeatureFlags enum.
+    BluetoothAudio(
+            R.bool.azure_communication_ui_feature_flag_bluetooth_audio,
+            R.string.azure_communication_ui_feature_flag_bluetooth_audio_label
+    ),
+    ScreenShareZoom(
+            R.bool.azure_communication_ui_feature_screen_share_zoom,
+            R.string.azure_communication_ui_feature_screen_share_zoom_label
+            );
+
+    FeatureFlags(int defaultBooleanId, int labelId) {
+        this.defaultBooleanId = defaultBooleanId;
+        this.labelId = labelId;
+        this.onStart = (application)->{};
+        this.onEnd = (application)->{};
+    }
+
+    // Id of the bool resource containing the default
+    int defaultBooleanId;
+
+    // Label to display on screen
+    int labelId;
+
+    Consumer<Application> onStart;
+    Consumer<Application> onEnd;
+
+    // ---------------------------- End Global Features ---------------------------------------------
+
+    // FeatureFlag Interface/Companion
+    //
+    // 1) `registerAdditionalEntry(FeatureFlag)` to add optional features at runtime
+    // 2) `initialize(context)` at app/activity start to initialize/start the system
+    // 3) `features` to get the list of all the available Feature Flags
+
+        private static Application applicationContext;
+        private static SharedPreferences sharedPrefs;
+
+        // Key for the SharedPrefs store that will be used for FeatureFlags
+        static final String FEATURE_FLAG_SHARED_PREFS_KEY = "FeatureFlags";
+
+        // Ensure this has an ApplicationContext to get SharedPreferences from
+        // This will allow us to access the current value and also the Resources to read the default
+        static void initialize(Context context) {
+
+            applicationContext = (Application) context.getApplicationContext();
+            sharedPrefs = applicationContext.getSharedPreferences(
+                FEATURE_FLAG_SHARED_PREFS_KEY,
+                Context.MODE_PRIVATE
+            );
+
+            // Start default features
+            for (FeatureFlag feature:features()) {
+
+            }
+            //features().filter { it.active }.forEach { it.onStart(applicationContext) }
+        }
+
+        private static ArrayList<FeatureFlag> additionalEntries = new ArrayList<>();
+
+        static boolean isActive(FeatureFlag flag) {
+            // If not added to the system, return false
+            if (!FeatureFlags.features().contains(flag)) {
+                return false;
+            }
+
+            String key = ""+flag.defaultBooleanId();
+            return FeatureFlags.sharedPrefs.getBoolean(
+                    key,
+                    FeatureFlags.applicationContext.getResources().getBoolean(flag.defaultBooleanId())
+            );
+        }
+
+        static void setActive(FeatureFlag flag, boolean value) {
+            final boolean wasActive = isActive(flag);
+            FeatureFlags.sharedPrefs.edit().putBoolean(""+flag.defaultBooleanId(), value).apply();
+            if (value != wasActive) {
+                // Toggled
+                if (value) {
+                    flag.onStart(FeatureFlags.applicationContext);
+                } else {
+                    flag.onEnd(FeatureFlags.applicationContext);
+                }
+            }
+        }
+
+        static void registerAdditionalFeature(FeatureFlag feature) {
+            if (!additionalEntries.contains(feature)) {
+                additionalEntries.add(feature);
+            }
+        }
+
+        static List<FeatureFlag> features() {
+            final List<FeatureFlag> list = new ArrayList<FeatureFlag>();
+            list.addAll(additionalEntries);
+            for (FeatureFlag feature:values()) {
+                list.add(feature);
+            }
+            return list;
+        }
+
+    @Override
+    public int defaultBooleanId() {
+        return defaultBooleanId;
+    }
+
+    @Override
+    public int labelId() {
+        return labelId;
+    }
+
+    @Override
+    public void onStart(Application application) {}
+
+    @Override
+    public void onEnd(Application application) {}
+
+}
+
+
+// Class to add additional Entries to the FeatureFlag system (e.g. from the Demo, or another app)
+class FeatureFlagEntry implements FeatureFlag {
+    final int defaultBooleanId;
+    final int labelId;
+    final FeatureFlagAppHook start;
+    final FeatureFlagAppHook end;
+
+    FeatureFlagEntry(int defaultBooleanId, int labelId, FeatureFlagAppHook start, FeatureFlagAppHook end) {
+        this.defaultBooleanId = defaultBooleanId;
+        this.labelId = labelId;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public int defaultBooleanId() {
+        return 0;
+    }
+
+    @Override
+    public int labelId() {
+        return 0;
+    }
+
+    @Override
+    public void onStart(Application application) {
+        if (start != null) start.call(application);
+    }
+
+    @Override
+    public void onEnd(Application application) {
+        if (end != null) end.call(application);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FeatureFlagEntry that = (FeatureFlagEntry) o;
+        return defaultBooleanId == that.defaultBooleanId && labelId == that.labelId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(defaultBooleanId, labelId);
+    }
+}
+
+interface FeatureFlagAppHook {
+    void call(Application application);
+}
+// A Feature Flag
+// This interface is shared between the Optional and Enum features
+interface FeatureFlag {
+    int defaultBooleanId();
+    int labelId();
+    void onStart(Application application);
+    void onEnd(Application application);
+}
+
