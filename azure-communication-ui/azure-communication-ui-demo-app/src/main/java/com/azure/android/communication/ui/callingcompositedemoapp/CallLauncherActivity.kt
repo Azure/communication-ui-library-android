1bb6fb9a49c874e6ceaaafc0a82cb00343dfdb17
ShaunaSong <93549644+ShaunaSong@users.noreply.github.com>
2022-03-14 12:31:42 -0300

    Merge Dev/Beta

--- azure-communication-ui/azure-communication-ui-demo-app/src/main/java/com/azure/android/communication/ui/callingcompositedemoapp/CallLauncherActivity.kt
+++ azure-communication-ui/azure-communication-ui-demo-app/src/main/java/com/azure/android/communication/ui/callingcompositedemoapp/CallLauncherActivity.kt
@@ -16,8 +16,6 @@ import com.azure.android.communication.ui.callingcompositedemoapp.features.Addit
 import com.azure.android.communication.ui.callingcompositedemoapp.features.conditionallyRegisterDiagnostics
 import com.azure.android.communication.ui.callingcompositedemoapp.launcher.CallingCompositeLauncher
 import com.azure.android.communication.ui.utilities.implementation.FeatureFlags
-import com.azure.android.communication.ui.callingcompositedemoapp.launcher.FeatureFlagView
-import com.azure.android.communication.ui.utilities.implementation.FeatureFlags
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
