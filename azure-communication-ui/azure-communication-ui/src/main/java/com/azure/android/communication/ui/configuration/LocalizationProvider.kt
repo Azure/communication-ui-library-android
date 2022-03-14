da5d54edc97e29dc40322a9c83026191720ec68f
Mohtasim <mbellah@microsoft.com>
2022-03-14 11:38:18 -0700

    [Review Changes] Adding documentation for the internal interfaces.
    Signed-off-by: Mohtasim <mbellah@microsoft.com>

--- azure-communication-ui/azure-communication-ui/src/main/java/com/azure/android/communication/ui/configuration/LocalizationProvider.kt
+++ azure-communication-ui/azure-communication-ui/src/main/java/com/azure/android/communication/ui/configuration/LocalizationProvider.kt
@@ -3,6 +3,11 @@
 
 package com.azure.android.communication.ui.configuration
 
+// Localization Provider applies the localization configuration in the library system
+// Makes the AppLocalizationProvider scalable for further feature addition
+// LocalizationConfiguration contains the locale, customStrings and RTL settings options.
+// For implementation
+// @see {@link com.azure.android.communication.ui.configuration.AppLocalizationProvider}
 internal interface LocalizationProvider {
     fun apply(localeConfig: LocalizationConfiguration)
     fun getLocalizedString(localeKey: String, sdkLocale: String): String
--- azure-communication-ui/azure-communication-ui/src/main/java/com/azure/android/communication/ui/configuration/SupportedLanguageAttributes.kt
+++ azure-communication-ui/azure-communication-ui/src/main/java/com/azure/android/communication/ui/configuration/SupportedLanguageAttributes.kt
@@ -3,6 +3,9 @@
 
 package com.azure.android.communication.ui.configuration
 
+// Supported Language attributes enables the extensible enum to have public method properties
+// For Implementation
+// @see {@link com.azure.android.communication.ui.configuration}
 internal interface SupportedLanguageAttributes {
     fun getLanguageCode(language: SupportedLanguages): String
     fun getIsRTLDefaultValue(language: SupportedLanguages): Boolean
