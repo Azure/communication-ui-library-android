a1f5d9f2848bb980280edfd840dead0091f65f22
Mohtasim <mbellah@microsoft.com>
2022-03-14 18:38:13 -0700

    Documentation format changed according to template[summery, description, link]
    Signed-off-by: Mohtasim <mbellah@microsoft.com>

--- azure-communication-ui/azure-communication-ui/src/main/java/com/azure/android/communication/ui/configuration/LocalizationProvider.kt
+++ azure-communication-ui/azure-communication-ui/src/main/java/com/azure/android/communication/ui/configuration/LocalizationProvider.kt
@@ -4,9 +4,12 @@
 package com.azure.android.communication.ui.configuration
 
 // Localization Provider applies the localization configuration in the library system
-// Makes the AppLocalizationProvider scalable for further feature addition
-// LocalizationConfiguration contains the locale, customStrings and RTL settings options.
+//
+// @method {@apply Applies the custom locale settings received from LocalizationConfiguration}
+// @method (@getLocalizedString Provides the string value for a specific key}
+//
 // For implementation
+//
 // @see {@link com.azure.android.communication.ui.configuration.AppLocalizationProvider}
 internal interface LocalizationProvider {
     fun apply(localeConfig: LocalizationConfiguration)
