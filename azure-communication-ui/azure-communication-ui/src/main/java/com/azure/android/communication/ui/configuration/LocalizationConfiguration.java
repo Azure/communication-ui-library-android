a22635a8f4693b8d2fbb409eba29c66e1b90a0d6
Mohtasim <99507832+Mohtasim@users.noreply.github.com>
2022-03-30 13:24:53 -0700

    [Refactor] Refactoring and code clean up of supported language related files (#149)

--- azure-communication-ui/azure-communication-ui/src/main/java/com/azure/android/communication/ui/configuration/LocalizationConfiguration.java
+++ azure-communication-ui/azure-communication-ui/src/main/java/com/azure/android/communication/ui/configuration/LocalizationConfiguration.java
@@ -6,7 +6,7 @@ package com.azure.android.communication.ui.configuration;
 
 import com.azure.android.communication.ui.CallComposite;
 
-import java.util.List;
+import java.util.Collection;
 
 /**
  * Localization configuration to provide for CallComposite.
@@ -15,10 +15,10 @@ import java.util.List;
  *
  * &#47;&#47; Initialize the call composite builder with different parameters
  * final CallCompositeBuilder builder = new CallCompositeBuilder&#40;&#41;
- *     .customizeLocalization&#40;new LocalizationConfiguration&#40;languageCode&#41;&#41;;
+ *     .localization&#40;new LocalizationConfiguration&#40;LanguageCode.FRENCH&#41;&#41;;
  *
  * final CallCompositeBuilder builder = new CallCompositeBuilder&#40;&#41;
- *     .customizeLocalization&#40;new LocalizationConfiguration&#40;languageCode, layoutDirection&#41;&#41;;
+ *     .localization&#40;new LocalizationConfiguration&#40;LanguageCode.FRENCH, layoutDirection&#41;&#41;;
  *
  * &#47;&#47; Build the call composite
  * CallComposite callComposite = builder.build&#40;&#41;;
@@ -28,16 +28,18 @@ import java.util.List;
  * @see CallComposite
  */
 public class LocalizationConfiguration {
-    private final String languageCode;
+    private final LanguageCode languageCode;
     private int layoutDirection;
 
     /**
      * Create Localization configuration.
      *
-     * @param languageCode    string eg,. "en"
+     * Set {@link LanguageCode};.
+     *
+     * @param languageCode    The {@link LanguageCode}; eg,. LanguageCode.FRENCH
      * @param layoutDirection layout direction eg,. LayoutDirection.RTL
      */
-    public LocalizationConfiguration(final String languageCode, final int layoutDirection) {
+    public LocalizationConfiguration(final LanguageCode languageCode, final int layoutDirection) {
         this.languageCode = languageCode;
         this.layoutDirection = layoutDirection;
     }
@@ -45,36 +47,36 @@ public class LocalizationConfiguration {
     /**
      * Create Localization configuration.
      *
-     * @param languageCode string eg,. "en"
+     * @param languageCode The {@link LanguageCode}; eg,. LanguageCode.FRENCH
      */
-    public LocalizationConfiguration(final String languageCode) {
+    public LocalizationConfiguration(final LanguageCode languageCode) {
         this.languageCode = languageCode;
     }
 
     /**
-     * Get current language String.
+     * Get current LanguageCode enum
      *
-     * @return language string
+     * @return The {@link LanguageCode};
      */
-    public String getLanguageCode() {
+    public LanguageCode getLanguageCode() {
         return languageCode;
     }
 
     /**
-     * Get isRightToLeft boolean.
+     * Get layoutDirection int.
      *
-     * @return isRightToLeft boolean
+     * @return layoutDirection int
      */
     public int layoutDirection() {
         return layoutDirection;
     }
 
     /**
-     * Get supported Locale string
+     * Get supported Locale list
      *
-     * @return List of supported Locale as String List
+     * @return List of supported languageCode as {@link LanguageCode}; List
      */
-    public static List<String> getSupportedLanguages() {
-        return AppLocalizationProvider.Companion.getSupportedLanguages();
+    public static Collection<LanguageCode> getSupportedLanguages() {
+        return LanguageCode.values();
     }
 }
