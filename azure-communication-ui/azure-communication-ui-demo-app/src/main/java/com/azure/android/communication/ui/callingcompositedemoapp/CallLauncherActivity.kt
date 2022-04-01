9217ebac3998d856c6546363a45b2103b9717157
Pavel Prystinka <pprystinka@microsoft.com>
2022-04-01 14:43:15 -0700

    Receive updated version from app center

--- azure-communication-ui/azure-communication-ui-demo-app/src/main/java/com/azure/android/communication/ui/callingcompositedemoapp/CallLauncherActivity.kt
+++ azure-communication-ui/azure-communication-ui-demo-app/src/main/java/com/azure/android/communication/ui/callingcompositedemoapp/CallLauncherActivity.kt
@@ -19,6 +19,7 @@ import com.azure.android.communication.ui.utilities.implementation.FeatureFlags
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
+import com.microsoft.appcenter.distribute.Distribute
 import java.util.UUID
 
 class CallLauncherActivity : AppCompatActivity() {
@@ -34,7 +35,8 @@ class CallLauncherActivity : AppCompatActivity() {
                 application,
                 BuildConfig.APP_SECRET,
                 Analytics::class.java,
-                Crashes::class.java
+                Crashes::class.java,
+                Distribute::class.java
             )
         }
         // Register Memory Viewer with FeatureFlags
