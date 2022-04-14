4946218513c1acf6ab902813b2efa7f113dd59d4
ShaunaSong <93549644+ShaunaSong@users.noreply.github.com>
2022-04-14 12:22:00 -0300

    Fix format

--- azure-communication-ui/azure-communication-ui/src/main/java/com/azure/android/communication/ui/presentation/CallCompositeActivity.kt
+++ azure-communication-ui/azure-communication-ui/src/main/java/com/azure/android/communication/ui/presentation/CallCompositeActivity.kt
@@ -28,11 +28,9 @@ import com.azure.android.communication.ui.presentation.fragment.setup.SetupFragm
 import com.azure.android.communication.ui.presentation.navigation.BackNavigation
 import com.azure.android.communication.ui.redux.action.CallingAction
 import com.azure.android.communication.ui.redux.state.NavigationStatus
-import com.microsoft.fluentui.util.accessibilityManager
 import com.microsoft.fluentui.util.activity
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
-import java.util.*
 
 internal class CallCompositeActivity : AppCompatActivity() {
 
