5c92cb4e4cf3a85e24a24f67484bba717c6280d6
Inderpal Singh Aulakh <73618019+iaulakh@users.noreply.github.com>
2022-04-20 16:37:43 -0700

    Image test url for demo app

--- azure-communication-ui/azure-communication-ui-demo-app/src/main/java/com/azure/android/communication/ui/callingcompositedemoapp/CallLauncherActivityRemoteParticipantJoinedHandler.kt
+++ azure-communication-ui/azure-communication-ui-demo-app/src/main/java/com/azure/android/communication/ui/callingcompositedemoapp/CallLauncherActivityRemoteParticipantJoinedHandler.kt
@@ -4,6 +4,11 @@
 package com.azure.android.communication.ui.callingcompositedemoapp
 
 import android.graphics.BitmapFactory
+import com.azure.android.communication.common.CommunicationIdentifier
+import com.azure.android.communication.common.CommunicationUserIdentifier
+import com.azure.android.communication.common.MicrosoftTeamsUserIdentifier
+import com.azure.android.communication.common.PhoneNumberIdentifier
+import com.azure.android.communication.common.UnknownIdentifier
 import com.azure.android.communication.ui.CallComposite
 import com.azure.android.communication.ui.CallingEventHandler
 import com.azure.android.communication.ui.configuration.events.CommunicationUIRemoteParticipantJoinedEvent
@@ -15,15 +20,43 @@ class CallLauncherActivityRemoteParticipantJoinedHandler(
 ) :
     CallingEventHandler<CommunicationUIRemoteParticipantJoinedEvent> {
 
-    override fun handle(it: CommunicationUIRemoteParticipantJoinedEvent) {
-        Thread {
-            val url =
-                URL("https://dt2sdf0db8zob.cloudfront.net/wp-content/uploads/2019/12/9-Best-Online-Avatars-and-How-to-Make-Your-Own-for-Free-image1-5.png")
-            val bitMap = BitmapFactory.decodeStream(url.openConnection().getInputStream())
-            callComposite.setRemoteParticipantPersonaData(
-                it.communicationIdentifier,
-                CommunicationUIPersonaData(bitMap)
-            )
-        }.start()
+    override fun handle(event: CommunicationUIRemoteParticipantJoinedEvent) {
+        // This code is for testing purpose
+        // The avatar images can be hosted anywhere as per requirements
+        // For testing purpose, the images are hosted on Azure Storage in .png format
+        val imageTestUrl = BuildConfig.REMOTE_PARTICIPANT_AVATAR_TEST_URL
+        if (imageTestUrl.isNotEmpty()) {
+            Thread {
+                try {
+                    val id =
+                        getRemoteParticipantId(event.communicationIdentifier).filterNot { it == ":"[0] || it == "-"[0] }
+                    val url =
+                        URL("$imageTestUrl$id.png")
+                    val bitMap = BitmapFactory.decodeStream(url.openConnection().getInputStream())
+                    callComposite.setRemoteParticipantPersonaData(
+                        event.communicationIdentifier,
+                        CommunicationUIPersonaData(bitMap)
+                    )
+                } catch (e: Exception) {
+                }
+            }.start()
+        }
+    }
+
+    private fun getRemoteParticipantId(identifier: CommunicationIdentifier): String {
+        return when (identifier) {
+            is PhoneNumberIdentifier -> {
+                identifier.phoneNumber
+            }
+            is MicrosoftTeamsUserIdentifier -> {
+                identifier.userId
+            }
+            is CommunicationUserIdentifier -> {
+                identifier.id
+            }
+            else -> {
+                (identifier as UnknownIdentifier).id
+            }
+        }
     }
 }
