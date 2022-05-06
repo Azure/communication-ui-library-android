61ffa51df37c7a4a8f2b5050f7daae8ddda78b39
Dhiraj Gupta <v-dhigupta@microsoft.com>
2022-05-06 15:41:10 -0400

    Fixed wrong location of license comment

--- azure-communication-ui/azure-communication-ui/src/main/java/com/azure/android/communication/ui/utilities/BottomCellActionViewHolder.kt
+++ azure-communication-ui/azure-communication-ui/src/main/java/com/azure/android/communication/ui/utilities/BottomCellActionViewHolder.kt
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.azure.android.communication.ui.utilities
 
 import android.view.View
@@ -8,9 +11,6 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.accessibility.AccessibilityNodeInfoCompat
 import com.azure.android.communication.ui.R
 import com.microsoft.fluentui.persona.AvatarView
-
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
 
 internal class BottomCellActionViewHolder(itemView: View) : BottomCellViewHolder(itemView) {
     private val imageView: ImageView = itemView.findViewById(R.id.cell_icon)
