e243856355f06310986b18a772d51de1027e029c
Dhiraj Gupta <v-dhigupta@microsoft.com>
2022-05-06 16:23:24 -0400

    Removed unnecessary implementation

--- azure-communication-ui/azure-communication-ui/src/main/java/com/azure/android/communication/ui/utilities/BottomCellViewHolder.kt
+++ azure-communication-ui/azure-communication-ui/src/main/java/com/azure/android/communication/ui/utilities/BottomCellViewHolder.kt
@@ -13,20 +13,6 @@ import com.azure.android.communication.ui.R
 internal open class BottomCellViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private val title: TextView = itemView.findViewById(R.id.cell_text)
     open fun setCellData(bottomCellItem: BottomCellItem) {
-        itemView.accessibilityDelegate = object : View.AccessibilityDelegate() {
-            override fun onInitializeAccessibilityNodeInfo(
-                host: View?,
-                info: AccessibilityNodeInfo?
-            ) {
-                super.onInitializeAccessibilityNodeInfo(host, info)
-                print(info)
-            }
-
-            override fun onPopulateAccessibilityEvent(host: View?, event: AccessibilityEvent?) {
-                super.onPopulateAccessibilityEvent(host, event)
-                print(event)
-            }
-        }
         title.text = bottomCellItem.title
         title.contentDescription = bottomCellItem.contentDescription
     }
