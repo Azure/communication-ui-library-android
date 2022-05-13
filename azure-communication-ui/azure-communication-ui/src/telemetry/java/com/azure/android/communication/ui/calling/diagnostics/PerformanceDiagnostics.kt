ecb715ab6b139a2729f8e4ff21c625e06461d062
Pavel Prystinka <pprystinka@microsoft.com>
2022-05-12 17:17:20 -0700

    Code formatting

--- azure-communication-ui/azure-communication-ui/src/telemetry/java/com/azure/android/communication/ui/calling/diagnostics/PerformanceDiagnostics.kt
+++ azure-communication-ui/azure-communication-ui/src/telemetry/java/com/azure/android/communication/ui/calling/diagnostics/PerformanceDiagnostics.kt
@@ -1,12 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-
-
 package com.azure.android.communication.ui.calling.diagnostics
 
 class PerformanceDiagnostics {
-
 
     companion object {
         const val CALL_SCREEN_LOADING = "CALL_SCREEN_LOADING"
