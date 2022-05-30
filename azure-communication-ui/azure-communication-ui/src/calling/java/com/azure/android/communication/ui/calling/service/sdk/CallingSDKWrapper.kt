28a21586ab4e1ec257a58e6204f0fef418296ce9
Pavel Prystinka <pprystinka@microsoft.com>
2022-05-30 12:32:25 -0700

    Use private drop SDK

--- azure-communication-ui/azure-communication-ui/src/calling/java/com/azure/android/communication/ui/calling/service/sdk/CallingSDKWrapper.kt
+++ azure-communication-ui/azure-communication-ui/src/calling/java/com/azure/android/communication/ui/calling/service/sdk/CallingSDKWrapper.kt
@@ -10,9 +10,9 @@ import com.azure.android.communication.calling.CallAgent
 import com.azure.android.communication.calling.CallAgentOptions
 import com.azure.android.communication.calling.CallClient
 import com.azure.android.communication.calling.CallClientOptions
+import com.azure.android.communication.calling.CallDiagnosticsOptions
 import com.azure.android.communication.calling.CameraFacing
 import com.azure.android.communication.calling.DeviceManager
-import com.azure.android.communication.calling.DiagnosticOptions
 import com.azure.android.communication.calling.GroupCallLocator
 import com.azure.android.communication.calling.HangUpOptions
 import com.azure.android.communication.calling.JoinCallOptions
@@ -411,9 +411,9 @@ internal class CallingSDKWrapper(
         return CallClient(callClientOptions)
     }
 
-    private fun getOrCreateDiagnosticOptions(callClientOptions: CallClientOptions): DiagnosticOptions {
+    private fun getOrCreateDiagnosticOptions(callClientOptions: CallClientOptions): CallDiagnosticsOptions {
         if (callClientOptions.diagnostics == null) {
-            callClientOptions.diagnostics = DiagnosticOptions()
+            callClientOptions.diagnostics = CallDiagnosticsOptions()
         }
 
         return callClientOptions.diagnostics
