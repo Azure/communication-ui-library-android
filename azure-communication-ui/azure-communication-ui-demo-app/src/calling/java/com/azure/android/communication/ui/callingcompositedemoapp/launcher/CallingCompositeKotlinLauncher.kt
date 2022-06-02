6d68b52bf9440d1e8b9748266edb7a5f5c32c5f0
Pavel Prystinka <pprystinka@microsoft.com>
2022-06-02 08:24:58 -0700

    Code formatting

--- azure-communication-ui/azure-communication-ui-demo-app/src/calling/java/com/azure/android/communication/ui/callingcompositedemoapp/launcher/CallingCompositeKotlinLauncher.kt
+++ azure-communication-ui/azure-communication-ui-demo-app/src/calling/java/com/azure/android/communication/ui/callingcompositedemoapp/launcher/CallingCompositeKotlinLauncher.kt
@@ -7,7 +7,12 @@ import com.azure.android.communication.common.CommunicationTokenCredential
 import com.azure.android.communication.common.CommunicationTokenRefreshOptions
 import com.azure.android.communication.ui.calling.CallComposite
 import com.azure.android.communication.ui.calling.CallCompositeBuilder
-import com.azure.android.communication.ui.calling.models.*
+import com.azure.android.communication.ui.calling.models.CallCompositeGroupCallLocator
+import com.azure.android.communication.ui.calling.models.CallCompositeJoinMeetingLocator
+import com.azure.android.communication.ui.calling.models.CallCompositeLocalOptions
+import com.azure.android.communication.ui.calling.models.CallCompositeLocalizationOptions
+import com.azure.android.communication.ui.calling.models.CallCompositeRemoteOptions
+import com.azure.android.communication.ui.calling.models.CallCompositeTeamsMeetingLinkLocator
 import com.azure.android.communication.ui.callingcompositedemoapp.CallLauncherActivity
 import com.azure.android.communication.ui.callingcompositedemoapp.CallLauncherActivityErrorHandler
 import com.azure.android.communication.ui.callingcompositedemoapp.R
@@ -61,8 +66,8 @@ class CallingCompositeKotlinLauncher(private val tokenRefresher: Callable<String
             CommunicationTokenCredential(communicationTokenRefreshOptions)
 
         val locator: CallCompositeJoinMeetingLocator =
-                if (groupId != null) CallCompositeGroupCallLocator(groupId)
-                else CallCompositeTeamsMeetingLinkLocator(meetingLink)
+            if (groupId != null) CallCompositeGroupCallLocator(groupId)
+            else CallCompositeTeamsMeetingLinkLocator(meetingLink)
 
         val remoteOptions = CallCompositeRemoteOptions(locator, communicationTokenCredential, displayName)
 
