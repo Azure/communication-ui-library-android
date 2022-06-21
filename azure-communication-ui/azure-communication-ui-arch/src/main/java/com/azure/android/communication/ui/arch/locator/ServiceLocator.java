e3d3a1fe63b3986e8d3750ab55fe13008a2a2e75
adamhammer <adamhammer@microsoft.com>
2022-06-21 11:58:24 -0700

    renamed DI to Locator

--- azure-communication-ui/azure-communication-ui-arch/src/main/java/com/azure/android/communication/ui/arch/locator/ServiceLocator.java
+++ azure-communication-ui/azure-communication-ui-arch/src/main/java/com/azure/android/communication/ui/arch/locator/ServiceLocator.java
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.android.communication.ui.arch.di;
+package com.azure.android.communication.ui.arch.locator;
 
 
 import java.util.HashMap;
