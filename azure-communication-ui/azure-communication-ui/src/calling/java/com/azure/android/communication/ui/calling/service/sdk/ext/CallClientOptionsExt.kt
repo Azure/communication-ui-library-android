256ed4909de2dd69513a4c9a3fe4c424b5951f27
Grisha Kruglov <grigory@kruglov.ca>
2022-06-27 21:43:06 -0700

    [Refactoring] Various refactorings toward being able to mock calling SDK (#367)

---
+++ azure-communication-ui/azure-communication-ui/src/calling/java/com/azure/android/communication/ui/calling/service/sdk/ext/CallClientOptionsExt.kt
@@ -0,0 +1,25 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.android.communication.ui.calling.service.sdk.ext
+
+import com.azure.android.communication.calling.CallClientOptions
+import com.azure.android.communication.calling.CallDiagnosticsOptions
+import com.azure.android.communication.ui.calling.logger.Logger
+
+internal fun CallClientOptions.getOrCreateDiagnostics(): CallDiagnosticsOptions {
+    if (this.diagnostics == null) {
+        this.diagnostics = CallDiagnosticsOptions()
+    }
+    return this.diagnostics
+}
+
+internal fun CallClientOptions.setTags(tags: Array<String>?, logger: Logger?): CallClientOptions {
+    tags?.let { this.getOrCreateDiagnostics().also { it.tags = it.tags + tags } }
+    logger?.let {
+        this.diagnostics.tags.forEach {
+            logger.debug("diagnostic tag element: $it")
+        }
+    }
+    return this
+}
