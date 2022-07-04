1c8798c749cc64776c14d36575403be3e237ec03
adamhammer <adamhammer@microsoft.com>
2022-07-04 12:46:26 -0700

    unit tests for AudioSwitchingMiddleware completed

--- azure-communication-ui/azure-communication-ui/src/testCalling/java/com/azure/android/communication/ui/calling/redux/middleware/audio/AudioSwitchingMiddlewareTest.kt
+++ azure-communication-ui/azure-communication-ui/src/testCalling/java/com/azure/android/communication/ui/calling/redux/middleware/audio/AudioSwitchingMiddlewareTest.kt
@@ -3,6 +3,7 @@ package com.azure.android.communication.ui.calling.redux.middleware.audio
 import android.os.Handler
 import com.azure.android.communication.ui.calling.redux.AppStore
 import com.azure.android.communication.ui.calling.redux.action.Action
+import com.azure.android.communication.ui.calling.redux.action.LifecycleAction
 import com.azure.android.communication.ui.calling.redux.action.LocalParticipantAction
 import com.azure.android.communication.ui.calling.redux.reducer.Reducer
 import com.azure.android.communication.ui.calling.redux.state.*
@@ -17,6 +18,7 @@ import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.kotlin.any
+import org.mockito.kotlin.mock
 
 @RunWith(MockitoJUnitRunner::class)
 class AudioSwitchingMiddlewareTest {
@@ -33,7 +35,6 @@ class AudioSwitchingMiddlewareTest {
         var bluetoothConnected = false
         var speakerPhoneConnected = false
         var earpieceConnected = false
-
 
         override fun disconnectAudio() {
             disconnectedAudio = true
@@ -67,6 +68,7 @@ class AudioSwitchingMiddlewareTest {
         }
     }
 
+    private val mockAudioSwitchingAdapter = MockAudioSwitchingAdapter()
 
     private lateinit var store : AppStore<ReduxState>
     private var lastActions = mutableListOf<Action>()
@@ -74,13 +76,16 @@ class AudioSwitchingMiddlewareTest {
 
     private fun setupStoreAndReducer(initialState: AppReduxState) {
         lastActions.clear()
-        val audioSwitchingMiddleware = AudioSwitchingMiddleware(MockAudioSwitchingAdapter())
+
+        mockAudioSwitchingAdapter.disconnectAudio()
+        val audioSwitchingMiddleware = AudioSwitchingMiddleware(mockAudioSwitchingAdapter)
         val testReducer = object : Reducer<ReduxState> {
             override fun reduce(state: ReduxState, action: Action): ReduxState {
                 lastActions.add(action)
                 return state
             }
         }
+
 
         Mockito.`when`(mockHandler.post(any())).thenAnswer(HandlerAnswerStub())
         Mockito.`when`(mockStoreHandlerThread.startHandlerThread()).thenReturn(mockHandler)
@@ -138,5 +143,102 @@ class AudioSwitchingMiddlewareTest {
         // Verify we requested "Receiver" as the previous device
         val deviceRequest = lastActions[1] as LocalParticipantAction.AudioDeviceChangeRequested
         assert (deviceRequest.requestedAudioDevice == AudioDeviceSelectionStatus.RECEIVER_SELECTED)
+    }
+
+
+
+    @Test
+    fun comeToForegroundWhenNotConnected() {
+        /// When we come to the foreground not in a connected state, we should
+        /// connect audio
+        setupStoreAndReducer(AppReduxState("TestDisplayName").apply {
+            callState = CallingState(CallingStatus.EARLY_MEDIA)
+            localParticipantState = LocalUserState(
+                displayName = "",
+                videoStreamID = null,
+                cameraState = CameraState(
+                    device = CameraDeviceSelectionStatus.FRONT,
+                    operation = CameraOperationalStatus.OFF,
+                    transmission = CameraTransmissionStatus.LOCAL,
+                ),
+                audioState = AudioState(
+                    operation = AudioOperationalStatus.OFF,
+                    device = AudioDeviceSelectionStatus.SPEAKER_SELECTED,
+                    bluetoothState = BluetoothState(
+                        available = false,
+                        deviceName = "Test"
+                    ),
+                ),
+            )
+        })
+
+        mockAudioSwitchingAdapter.disconnectAudio()
+        assert(mockAudioSwitchingAdapter.disconnectedAudio)
+        store.dispatch(LifecycleAction.EnterForegroundSucceeded())
+        assert(mockAudioSwitchingAdapter.speakerPhoneConnected)
+    }
+
+
+
+    @Test
+    fun gotoBackgroundWhenNotConnected() {
+        /// When we go to the background not in a connected state, we should
+        /// disconnect audio
+        setupStoreAndReducer(AppReduxState("TestDisplayName").apply {
+            callState = CallingState(CallingStatus.EARLY_MEDIA)
+            localParticipantState = LocalUserState(
+                displayName = "",
+                videoStreamID = null,
+                cameraState = CameraState(
+                    device = CameraDeviceSelectionStatus.FRONT,
+                    operation = CameraOperationalStatus.OFF,
+                    transmission = CameraTransmissionStatus.LOCAL,
+                ),
+                audioState = AudioState(
+                    operation = AudioOperationalStatus.OFF,
+                    device = AudioDeviceSelectionStatus.SPEAKER_SELECTED,
+                    bluetoothState = BluetoothState(
+                        available = false,
+                        deviceName = "Test"
+                    ),
+                ),
+            )
+        })
+
+        mockAudioSwitchingAdapter.enableSpeakerPhone()
+        assert(!mockAudioSwitchingAdapter.disconnectedAudio)
+        store.dispatch(LifecycleAction.EnterBackgroundSucceeded())
+        assert(mockAudioSwitchingAdapter.disconnectedAudio)
+    }
+
+
+    @Test
+    fun testBasicSwitching() {
+        /// When we go to the background not in a connected state, we should
+        /// disconnect audio
+        setupStoreAndReducer(AppReduxState("TestDisplayName").apply {
+            callState = CallingState(CallingStatus.CONNECTED)
+            localParticipantState = LocalUserState(
+                displayName = "",
+                videoStreamID = null,
+                cameraState = CameraState(
+                    device = CameraDeviceSelectionStatus.FRONT,
+                    operation = CameraOperationalStatus.OFF,
+                    transmission = CameraTransmissionStatus.LOCAL,
+                ),
+                audioState = AudioState(
+                    operation = AudioOperationalStatus.OFF,
+                    device = AudioDeviceSelectionStatus.SPEAKER_SELECTED,
+                    bluetoothState = BluetoothState(
+                        available = false,
+                        deviceName = "Test"
+                    ),
+                ),
+            )
+        })
+
+        store.dispatch(LocalParticipantAction.AudioDeviceChangeRequested(requestedAudioDevice = AudioDeviceSelectionStatus.RECEIVER_SELECTED))
+        assert(lastActions[0] is LocalParticipantAction.AudioDeviceChangeRequested)
+        assert(mockAudioSwitchingAdapter.earpieceConnected)
     }
 }
