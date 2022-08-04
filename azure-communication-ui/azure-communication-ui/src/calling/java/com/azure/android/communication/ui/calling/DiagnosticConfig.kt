0d1769d52afdb6751cdd3431ec4f9d9a45abcb62
Grisha Kruglov <grigory@kruglov.ca>
2022-08-04 10:45:38 -0700

    Document template of the application ID as it is used for telemetry purposes (#382)

--- azure-communication-ui/azure-communication-ui/src/calling/java/com/azure/android/communication/ui/calling/DiagnosticConfig.kt
+++ azure-communication-ui/azure-communication-ui/src/calling/java/com/azure/android/communication/ui/calling/DiagnosticConfig.kt
@@ -8,6 +8,17 @@ internal class DiagnosticConfig {
 
     private fun getApplicationId(): String {
         val callingCompositeVersionName = "1.0.0"
-        return "aca110/$callingCompositeVersionName"
+        val baseTag = "ac"
+        // Tag template is: acXYYY/<version>
+        // Where:
+        // - X describes a platform, [r: web, i: iOS, a: Android]
+        // - YYY describes what's running on this platform (optional):
+        //      Y[0] is high-level artifact,
+        //          [0: undefined, 1: AzureCommunicationLibrary, 2: ACS SampleApp]
+        //      Y[1] is specific implementation,
+        //          [0: undefined, 1: Call Composite, 2: Chat Composite, 3: CallWithChatComposite, 4: UI Components]
+        //      Y[2] is reserved for implementation details,
+        //          [0: undefined]
+        return "${baseTag}a110/$callingCompositeVersionName"
     }
 }
