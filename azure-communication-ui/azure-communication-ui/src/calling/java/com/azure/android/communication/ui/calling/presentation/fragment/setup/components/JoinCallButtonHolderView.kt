8f5177865aceb5a3ec02f5c9cab6d999d7c06bc6
Mrayyan <t-musiddiqui@microsoft.com>
2022-08-17 22:59:12 -0700

    Added network connection error

--- azure-communication-ui/azure-communication-ui/src/calling/java/com/azure/android/communication/ui/calling/presentation/fragment/setup/components/JoinCallButtonHolderView.kt
+++ azure-communication-ui/azure-communication-ui/src/calling/java/com/azure/android/communication/ui/calling/presentation/fragment/setup/components/JoinCallButtonHolderView.kt
@@ -4,18 +4,30 @@
 package com.azure.android.communication.ui.calling.presentation.fragment.setup.components
 
 import android.content.Context
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.util.AttributeSet
+import android.view.View
 import android.view.accessibility.AccessibilityEvent
+import android.view.accessibility.AccessibilityNodeInfo
 import android.widget.Button
 import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import com.azure.android.communication.ui.R
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.microsoft.fluentui.snackbar.Snackbar
 
 internal class JoinCallButtonHolderView : ConstraintLayout {
     constructor(context: Context) : super(context)
@@ -28,6 +40,9 @@ internal class JoinCallButtonHolderView : ConstraintLayout {
     private lateinit var joiningCallText: AppCompatTextView
 
     private lateinit var viewModel: JoinCallButtonHolderViewModel
+
+    private lateinit var snackBar: Snackbar
+    private lateinit var snackBarTextView: TextView
 
     override fun onFinishInflate() {
         super.onFinishInflate()
@@ -51,8 +66,14 @@ internal class JoinCallButtonHolderView : ConstraintLayout {
         joiningCallText.text = context.getString(R.string.azure_communication_ui_calling_setup_view_button_connecting_call)
 
         setupJoinCallButton.setOnClickListener {
-            viewModel.launchCallScreen()
+            if (isNetworkConnectionAvailable()) {
+                viewModel.launchCallScreen()
+            } else {
+                initSnackBar()
+                displaySnackBar()
+            }
         }
+
         viewLifecycleOwner.lifecycleScope.launch {
             viewModel.getJoinCallButtonEnabledFlow().collect {
                 onJoinCallEnabledChanged(it)
@@ -63,9 +84,77 @@ internal class JoinCallButtonHolderView : ConstraintLayout {
         }
     }
 
+    fun stop() {
+        if (snackBar.isShown) {
+            snackBar.dismiss()
+        }
+        rootView.invalidate()
+        // to fix memory leak
+        snackBar.anchorView = null
+    }
+
     private fun onJoinCallEnabledChanged(isEnabled: Boolean) {
         setupJoinCallButton.isEnabled = isEnabled
         setupJoinCallButtonText.isEnabled = isEnabled
+    }
+
+    private fun displaySnackBar() {
+        val errorMessage = context.getString(R.string.azure_communication_ui_calling_no_connection_available)
+        if (errorMessage.isBlank()) return
+        snackBarTextView.text = errorMessage
+        snackBar.run {
+            if (isShown) {
+                dismiss()
+            }
+            show()
+
+            view.contentDescription =
+                "${context.getString(R.string.azure_communication_ui_calling_alert_title)}: $errorMessage"
+            view.accessibilityFocus()
+        }
+    }
+
+    private fun initSnackBar() {
+        snackBar = Snackbar.make(
+            rootView,
+            "",
+            Snackbar.LENGTH_INDEFINITE,
+            Snackbar.Style.REGULAR
+        ).apply {
+            animationMode = BaseTransientBottomBar.ANIMATION_MODE_FADE
+            setAction(rootView.context!!.getText(R.string.azure_communication_ui_calling_snack_bar_button_dismiss)) {}
+            anchorView =
+                rootView.findViewById(R.id.azure_communication_ui_setup_join_call_button)
+            view.background.colorFilter = PorterDuffColorFilter(
+                ContextCompat.getColor(
+                    rootView.context,
+                    R.color.azure_communication_ui_calling_color_snack_bar_background
+                ),
+                PorterDuff.Mode.SRC_IN
+            )
+            snackBarTextView = view.findViewById(R.id.snackbar_text)
+            snackBarTextView.setTextColor(
+                ContextCompat.getColor(
+                    rootView.context,
+                    R.color.azure_communication_ui_calling_color_snack_bar_text_color
+                )
+            )
+            view.findViewById<AppCompatButton>(R.id.snackbar_action).apply {
+                setTextColor(
+                    ContextCompat.getColor(
+                        rootView.context,
+                        R.color.azure_communication_ui_calling_color_snack_bar_text_color
+                    )
+                )
+                isAllCaps = false
+                contentDescription =
+                    rootView.context.getText(R.string.azure_communication_ui_calling_snack_bar_button_dismiss)
+            }
+            ViewCompat.setImportantForAccessibility(
+                view,
+                ViewCompat.IMPORTANT_FOR_ACCESSIBILITY_YES
+            )
+        }
     }
 
     private fun onDisableJoinCallButtonChanged(isBlocked: Boolean) {
@@ -82,5 +171,42 @@ internal class JoinCallButtonHolderView : ConstraintLayout {
             progressBar.visibility = GONE
             joiningCallText.visibility = GONE
         }
+    }
+
+    private fun isNetworkConnectionAvailable(): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+
+            if (capabilities != null) {
+                return capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
+            }
+        } else {
+            val networkInfo = connectivityManager.activeNetworkInfo
+            networkInfo ?: return false
+            return networkInfo.isConnected && (
+                networkInfo.type == ConnectivityManager.TYPE_WIFI ||
+                    networkInfo.type == ConnectivityManager.TYPE_MOBILE
+                ) ||
+                networkInfo.type == ConnectivityManager.TYPE_ETHERNET
+        }
+
+        return false
+    }
+
+    private fun View.accessibilityFocus(): View {
+        post {
+            performAccessibilityAction(AccessibilityNodeInfo.ACTION_ACCESSIBILITY_FOCUS, null)
+            sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_SELECTED)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+                accessibilityTraversalAfter = R.id.azure_communication_ui_setup_audio_device_button
+                accessibilityTraversalBefore = R.id.azure_communication_ui_setup_join_call_holder
+            }
+        }
+        return this
     }
 }
