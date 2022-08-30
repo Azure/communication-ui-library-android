7406d3fe5acdee7a921c46ad8b52bad013572b96
Inderpal Singh Aulakh <73618019+iaulakh@users.noreply.github.com>
2022-08-30 15:11:47 -0700

    [Chat][Feature] add chat library structure

---
+++ azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/ChatCompositeException.java
@@ -0,0 +1,38 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.android.communication.ui.chat;
+
+import androidx.annotation.NonNull;
+
+/**
+ * Defines the base type of custom Exception that can be thrown by this Library.
+ */
+public final class ChatCompositeException extends RuntimeException {
+
+    /**
+     * Constructs a new Chat Composite exception with the specified error message and cause. Note
+     * that the error message associated with "cause" is not automatically incorporated into this
+     * exception's error message.
+     *
+     * @param errorMessage - the error message.  The error message can be retrieved by the
+     *                     getMessage() method
+     * @param cause - the cause (which is saved for later retrieval by the getCause() method).  A
+     *              null value is permitted, and indicates that the cause is non-existent or unknown.
+     */
+    public ChatCompositeException(final String errorMessage, @NonNull final Throwable cause) {
+        super(errorMessage, cause);
+    }
+    /**
+     * Constructs a new Chat Composite exception with the specified cause and  message of
+     * (cause==null ? null : cause.toString()) (which typically contains the class and detail message
+     * of cause). This constructor is useful for exceptions that are little more than wrappers for
+     * other throwables.
+     *
+     * @param cause - the cause (which is saved for later retrieval by the Throwable.getCause() method).
+     *              A null value is permitted, and indicates that the cause is nonexistent or unknown.
+     */
+    public ChatCompositeException(final Throwable cause) {
+        super(cause);
+    }
+}
---
+++ azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/models/ChatCompositeLocalOptions.java
@@ -0,0 +1,54 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.android.communication.ui.chat.models;
+
+import com.azure.android.communication.ui.chat.ChatComposite;
+
+/**
+ * ChatCompositeLocalOptions for ChatComposite.launch.
+ *
+ * <p>
+ *     Local Options for the Chat Composite. These options are not shared with the server and impact local views only.
+ *     E.g. The Local Participant Name if it differs from the display name you'd like to share with the server.
+ * </p>
+ * <pre>
+ *
+ * &#47;&#47; Initialize the chat composite builder
+ * final ChatCompositeBuilder builder = new ChatCompositeBuilder&#40;&#41;;
+ *
+ * &#47;&#47; Build the chat composite
+ * ChatComposite chatComposite = builder.build&#40;&#41;;
+ *
+ * &#47;&#47; Build the ChatCompositeLocalOptions with {@link ChatCompositeParticipantViewData}
+ * ChatCompositeLocalOptions localOptions = new ChatCompositeLocalOptions(
+ *     new ChatCompositeParticipantViewData&#40;...&#41);
+ *
+ * &#47;&#47; Launch chat
+ * chatComposite.launch&#40;.., .., localOptions&#41
+ * </pre>
+ *
+ * @see ChatComposite
+ */
+public final class ChatCompositeLocalOptions {
+    private final ChatCompositeParticipantViewData participantViewData;
+
+    /**
+     * Create Local Options.
+     *
+     * @param participantViewData The {@link ChatCompositeParticipantViewData};
+     * @see ChatCompositeParticipantViewData
+     */
+    public ChatCompositeLocalOptions(final ChatCompositeParticipantViewData participantViewData) {
+        this.participantViewData = participantViewData;
+    }
+
+    /**
+     * Get {@link ChatCompositeParticipantViewData}.
+     *
+     * @return The {@link ChatCompositeParticipantViewData};
+     */
+    public ChatCompositeParticipantViewData getParticipantViewData() {
+        return participantViewData;
+    }
+}
