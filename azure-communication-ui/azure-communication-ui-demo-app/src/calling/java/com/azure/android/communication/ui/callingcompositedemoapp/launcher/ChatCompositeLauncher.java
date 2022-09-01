6394cceb5f6f4b3ad1eca436d7d23b0a8d8f3547
Mrayyan <t-musiddiqui@microsoft.com>
2022-09-01 10:02:57 -0700

    Revert "Revert "Lauch chat composite""
    This reverts commit 45f09d82be6b974c401933709a039f352d977bfc.

---
+++ azure-communication-ui/azure-communication-ui-demo-app/src/calling/java/com/azure/android/communication/ui/callingcompositedemoapp/launcher/ChatCompositeLauncher.java
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.android.communication.ui.callingcompositedemoapp.launcher;
+
+import com.azure.android.communication.ui.callingcompositedemoapp.CallLauncherActivity;
+
+public interface ChatCompositeLauncher {
+    void launch(CallLauncherActivity callLauncherActivity);
+}
---
+++ azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/ChatCompositeBuilder.java
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.android.communication.ui.chat;
+
+public class ChatCompositeBuilder {
+    public ChatComposite build() {
+        return new ChatComposite();
+    }
+}
