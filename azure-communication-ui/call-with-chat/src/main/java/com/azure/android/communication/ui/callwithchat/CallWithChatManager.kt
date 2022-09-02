c376c49fdcb10736c19b8d8aa321b40dbf81826a
Pavel Prystinka <pprystinka@microsoft.com>
2022-09-02 14:35:30 -0700

    remove commented out code

--- azure-communication-ui/call-with-chat/src/main/java/com/azure/android/communication/ui/callwithchat/CallWithChatManager.kt
+++ azure-communication-ui/call-with-chat/src/main/java/com/azure/android/communication/ui/callwithchat/CallWithChatManager.kt
@@ -8,11 +8,6 @@ import com.azure.android.communication.ui.calling.CallComposite
 import com.azure.android.communication.ui.callwithchat.models.CallWithChatCompositeLocalOptions
 import com.azure.android.communication.ui.callwithchat.models.CallWithChatCompositeRemoteOptions
 import com.azure.android.communication.ui.chat.ChatComposite
-// import com.azure.android.communication.ui.chat.implementation.ChatServiceConfigurationImpl
-// import com.azure.android.communication.ui.chat.models.ChatCompositeJoinLocator
-// import com.azure.android.communication.ui.chat.models.ChatCompositeLocalOptions
-// import com.azure.android.communication.ui.chat.models.ChatCompositeParticipantViewData
-// import com.azure.android.communication.ui.chat.models.ChatCompositeRemoteOptions
 
 internal class CallWithChatManager(
     private val callComposite: CallComposite,
@@ -23,53 +18,5 @@ internal class CallWithChatManager(
         remoteOptions: CallWithChatCompositeRemoteOptions,
         localOptions: CallWithChatCompositeLocalOptions?
     ) {
-//        ChatServiceConfigurationImpl.usePolling = true
-//
-//        callingIntegrationBridge.addOnCallStaredEventHandler {
-//            chatComposite.launch(context, getChatRemoteOptions(remoteOptions), getChatLocalOptions(localOptions))
-//        }
-//
-//        chatComposite.addOnViewClosedEventHandler {
-//            callingIntegrationBridge.removeOverlay()
-//        }
-//
-//        callComposite.launch(context, getCallRemoteOptions(remoteOptions))
     }
-
-//    private fun getCallRemoteOptions(remoteOptions: CallWithChatCompositeRemoteOptions): CallCompositeRemoteOptions {
-//        val callLocator = if (remoteOptions.locator is CallWithChatCompositeCallAndChatLocator) {
-//            val groupId = (remoteOptions.locator as CallWithChatCompositeCallAndChatLocator).groupId
-//            CallCompositeGroupCallLocator(groupId)
-//        } else {
-//            val meetingLink = (remoteOptions.locator as CallWithChatCompositeTeamsMeetingLinkLocator).meetingLink
-//            CallCompositeTeamsMeetingLinkLocator(meetingLink)
-//        }
-//        return CallCompositeRemoteOptions(callLocator, remoteOptions.credential, remoteOptions.displayName)
-//    }
-//
-//    private fun getChatRemoteOptions(remoteOptions: CallWithChatCompositeRemoteOptions): ChatCompositeRemoteOptions {
-//        val threadId =
-//            if (remoteOptions.locator is CallWithChatCompositeCallAndChatLocator) {
-//                (remoteOptions.locator as CallWithChatCompositeCallAndChatLocator).chatThreadId
-//            } else {
-//                val locator = remoteOptions.locator as CallWithChatCompositeTeamsMeetingLinkLocator
-//                TeamsUrlParser.getThreadId(locator.meetingLink)
-//            }
-//
-//        val chatLocator = ChatCompositeJoinLocator(remoteOptions.locator.endpoint, threadId)
-//        return ChatCompositeRemoteOptions(
-//            chatLocator,
-//            remoteOptions.communicationIdentifier,
-//            remoteOptions.credential,
-//            remoteOptions.displayName,
-//            "", "", ""
-//        )
-//    }
-
-//    private fun getChatLocalOptions(localOptions: CallWithChatCompositeLocalOptions?): ChatCompositeLocalOptions {
-//        val participantViewData = ChatCompositeParticipantViewData()
-//        participantViewData.avatarBitmap = localOptions?.participantViewData?.avatarBitmap
-//
-//        return ChatCompositeLocalOptions(participantViewData).also { it.isBackgroundMode = true }
-//    }
 }
