91a5ab434574eae4ec377794e1b1e4f9b49496b3
Mrayyan <t-musiddiqui@microsoft.com>
2022-09-02 16:40:17 -0700

    Update ChatActivity.kt

--- azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/ChatActivity.kt
+++ azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/ChatActivity.kt
@@ -6,19 +6,13 @@ package com.azure.android.communication.ui.chat
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.text.BasicText
 
 class ChatActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = "Hello Chat!")
+            BasicText(text = "Hello Chat!")
         }
     }
 }
