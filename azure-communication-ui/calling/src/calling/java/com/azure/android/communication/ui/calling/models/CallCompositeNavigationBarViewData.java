4346bf7522c1c0964072c1cd5716c6b1fb83b482
pavelprystinka <30868871+pavelprystinka@users.noreply.github.com>
2022-09-02 12:41:50 -0700

    [Feature] Make Title a require field on the CallCompositeNavigationBarViewData (#407)

--- azure-communication-ui/calling/src/calling/java/com/azure/android/communication/ui/calling/models/CallCompositeNavigationBarViewData.java
+++ azure-communication-ui/calling/src/calling/java/com/azure/android/communication/ui/calling/models/CallCompositeNavigationBarViewData.java
@@ -4,55 +4,56 @@
 package com.azure.android.communication.ui.calling.models;
 
 /**
- * Provides navigation bar view data to Call Composite including title and subTitle
+ * Provides navigation bar view data to Call Composite including title and subtitle
  *
  * Create an instance of CallCompositeNavigationBarViewData and pass it to
  * {@link CallCompositeLocalOptions} when launching a new call
  *
  */
 public final class CallCompositeNavigationBarViewData {
-    private String callTitle = null;
-
-    private String callSubTitle = null;
+    private String title = null;
+    private String subtitle = null;
 
     /**
      * Constructs an empty {@link  CallCompositeNavigationBarViewData}
      */
-    public CallCompositeNavigationBarViewData() { }
+    public CallCompositeNavigationBarViewData(final String title) {
+        this.title = title;
+    }
 
     /**
      * Get the call title
      * @return The title of the call
      */
-    public String getCallTitle() {
-        return callTitle;
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Set the title of the call setup screen to the supplied String
+     * @param title Title of the call
+     * @return The current CallCompositeNavigationBarViewData for Fluent use
+     */
+    public CallCompositeNavigationBarViewData setTitle(final String title) {
+        this.title = title;
+        return this;
     }
 
     /**
      * Get the call sub title
      * @return The subtitle of the call
      */
-    public String getCallSubTitle() {
-        return callSubTitle;
-    }
-
-    /**
-     * Set the title of the call setup screen to the supplied String
-     * @param callTitle Title of the call
-     * @return The current CallCompositeNavigationBarViewData for Fluent use
-     */
-    public CallCompositeNavigationBarViewData setCallTitle(final String callTitle) {
-        this.callTitle = callTitle;
-        return this;
+    public String getSubtitle() {
+        return subtitle;
     }
 
     /**
      * Set the subtitle of the call setup screen to the supplied String
-     * @param callSubTitle Subtitle of the call
+     * @param subtitle Subtitle of the call
      * @return The current CallCompositeNavigationBarViewData for Fluent use
      */
-    public CallCompositeNavigationBarViewData setCallSubTitle(final String callSubTitle) {
-        this.callSubTitle = callSubTitle;
+    public CallCompositeNavigationBarViewData setSubtitle(final String subtitle) {
+        this.subtitle = subtitle;
         return this;
     }
 }
