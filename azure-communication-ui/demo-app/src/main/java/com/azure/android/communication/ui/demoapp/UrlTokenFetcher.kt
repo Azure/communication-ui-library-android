36237770c91e3c0e23bc092a7e38183ec4f4dbe0
pavelprystinka <30868871+pavelprystinka@users.noreply.github.com>
2022-09-06 10:43:40 -0700

    [Feature] CallWithChat demo app scaffolding (#411)

--- azure-communication-ui/demo-app/src/main/java/com/azure/android/communication/ui/demoapp/UrlTokenFetcher.kt
+++ azure-communication-ui/demo-app/src/main/java/com/azure/android/communication/ui/demoapp/UrlTokenFetcher.kt
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.android.communication.ui.callingcompositedemoapp
+package com.azure.android.communication.ui.demoapp
 
 import com.github.kittinunf.fuel.httpGet
 import org.json.JSONObject
