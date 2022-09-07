1e9f6a42945c8c5647770f773cbff5c5cd4dca2d
Mrayyan <t-musiddiqui@microsoft.com>
2022-09-07 16:12:16 -0700

    Demo app clean up

--- azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/ChatComposite.java
+++ azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/ChatComposite.java
@@ -6,13 +6,15 @@ package com.azure.android.communication.ui.chat;
 import android.content.Context;
 import android.content.Intent;
 
+import com.azure.android.communication.ui.chat.presentation.ChatCompositeActivity;
+
 public class ChatComposite {
 
     ChatComposite() {
     }
 
     public void launch(final Context context) {
-        final Intent launchIntent = new Intent(context, ChatActivity.class);
+        final Intent launchIntent = new Intent(context, ChatCompositeActivity.class);
         context.startActivity(launchIntent);
     }
 }
--- azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/presentation/ChatCompositeActivity.kt
+++ azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/presentation/ChatCompositeActivity.kt
@@ -4,15 +4,34 @@
 package com.azure.android.communication.ui.chat.presentation
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicText
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
-internal class ChatCompositeActivity : AppCompatActivity() {
+class ChatCompositeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BasicText(text = "Hello Chat!")
+            Column(
+                Modifier
+                    .background(Color.White)
+                    .fillMaxSize()
+                    .padding(8.dp)
+            ) {
+                BasicText(
+                    text = "Hello UI Library Chat!",
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                )
+            }
         }
     }
 }
