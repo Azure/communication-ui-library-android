3d8b3d056fcfdec3a3a4d4496eaa444fc8366c34
Mohtasim <mbellah@microsoft.com>
2022-09-13 13:59:12 -0700

    Refactor for Ktlint on actionbar
    Signed-off-by: Mohtasim <mbellah@microsoft.com>

--- azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/presentation/components/ChatCompsiteActionBar.kt
+++ azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/presentation/components/ChatCompsiteActionBar.kt
@@ -4,9 +4,9 @@
 package com.azure.android.communication.ui.chat.presentation.components
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
