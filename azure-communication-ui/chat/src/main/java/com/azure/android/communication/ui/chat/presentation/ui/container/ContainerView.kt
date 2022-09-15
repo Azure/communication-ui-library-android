d5793c3af4b585816686553b013234a2b5897a39
Mohtasim <mbellah@microsoft.com>
2022-09-15 15:52:18 -0700

    Copyright message correction
    Signed-off-by: Mohtasim <mbellah@microsoft.com>

--- azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/presentation/ui/container/ContainerView.kt
+++ azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/presentation/ui/container/ContainerView.kt
@@ -1,9 +1,5 @@
-/*
- * *
- *  * Copyright (c) Microsoft Corporation. All rights reserved.
- *  * Licensed under the MIT License.
- *
- */
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
 package com.azure.android.communication.ui.chat.presentation.ui.container
 
