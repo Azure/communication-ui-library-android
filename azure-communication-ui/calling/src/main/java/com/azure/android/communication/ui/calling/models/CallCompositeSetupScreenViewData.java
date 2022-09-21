b0171e6a55e6c871126b34df5910950cebcbcf0e
pavelprystinka <30868871+pavelprystinka@users.noreply.github.com>
2022-09-21 09:47:03 -0700

    [Calling][Feature] Rename NavigationBarViewData to SetupScreenViewData (#439)

---
+++ azure-communication-ui/calling/src/main/java/com/azure/android/communication/ui/calling/models/CallCompositeSetupScreenViewData.java
@@ -0,0 +1,60 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.android.communication.ui.calling.models;
+
+/**
+ * Provides navigation bar view data to Call Composite including title and subtitle.
+ *
+ * Create an instance of {@link CallCompositeSetupScreenViewData} and pass it to
+ * {@link CallCompositeLocalOptions} when launching a new call.
+ *
+ */
+public final class CallCompositeSetupScreenViewData {
+    private String title = null;
+    private String subtitle = null;
+
+    /**
+     * Constructs {@link  CallCompositeSetupScreenViewData}.
+     */
+    public CallCompositeSetupScreenViewData() {
+    }
+
+    /**
+     * Get the call title.
+     * @return The title of the call.
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Get the call subtitle.
+     * @return The subtitle of the call.
+     */
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    /**
+     * Set the title of the call setup screen to the supplied String.
+     * @param title title of the call.
+     * @return The current {@link CallCompositeSetupScreenViewData}.
+     */
+    public CallCompositeSetupScreenViewData setTitle(final String title) {
+        this.title = title;
+        return this;
+    }
+
+    /**
+     * Set the title and subtitle of the call setup screen to the supplied String.
+     * @param title title of the call.
+     * @param subtitle subtitle of the call.
+     * @return The current {@link CallCompositeSetupScreenViewData}.
+     */
+    public CallCompositeSetupScreenViewData setTitleAndSubtitle(final String title, final String subtitle) {
+        this.title = title;
+        this.subtitle = subtitle;
+        return this;
+    }
+}
