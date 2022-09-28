6f3d76a28222323b5c23e4da25f8d1d23b7ad5b8
Adam Hammer <adamhammer2@gmail.com>
2022-09-28 14:12:07 -0700

    [Feature][Chat] redux view model (#456)

--- azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/presentation/style/ChatCompositeChatTheme.kt
+++ azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/presentation/style/ChatCompositeChatTheme.kt
@@ -1,18 +1,17 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.android.communication.ui.chat.presentation.theme
+package com.azure.android.communication.ui.chat.presentation.style
 
 import androidx.compose.runtime.Composable
 import com.microsoft.fluentui.theme.FluentTheme
 import com.microsoft.fluentui.theme.ThemeMode
 
 @Composable
-fun ChatCompositeUITheme(
+internal fun ChatCompositeUITheme(
     themeMode: ThemeMode = ThemeMode.Auto,
     content: @Composable () -> Unit,
 ) {
-
     FluentTheme(
         themeMode = themeMode,
         content = content
--- azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/presentation/ui/chat/components/ChatCompsiteActionBar.kt
+++ azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/presentation/ui/chat/components/ChatCompsiteActionBar.kt
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.android.communication.ui.chat.presentation.components
+package com.azure.android.communication.ui.chat.presentation.ui.chat.components
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,7 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun ChatCompositeActionBar() {
+internal fun ChatCompositeActionBar() {
     Box(modifier = Modifier.fillMaxWidth()) {
         Row {}
     }
@@ -20,7 +20,7 @@ fun ChatCompositeActionBar() {
 
 @Preview
 @Composable
-fun PreviewChatCompsiteActionBar() {
+internal fun PreviewChatCompositeActionBar() {
     Column {
         ChatCompositeActionBar()
     }
