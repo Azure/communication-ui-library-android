fb06cf986cc5c9c4482da51b0f49881742ae322a
adamhammer <adamhammer@microsoft.com>
2022-09-28 12:46:44 -0700

    migrate from ChatMessage -> MessageInfoModel

--- azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/presentation/ui/view_model/ChatMessageViewModel.kt
+++
@@ -1,5 +0,0 @@
-package com.azure.android.communication.ui.chat.presentation.ui.view_model
-
-import com.azure.android.communication.chat.models.ChatMessage
-
-class ChatMessageViewModel(val chatMessage: ChatMessage)
---
+++ azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/presentation/ui/view_model/MessageViewModel.kt
@@ -0,0 +1,5 @@
+package com.azure.android.communication.ui.chat.presentation.ui.view_model
+
+import com.azure.android.communication.ui.chat.models.MessageInfoModel
+
+internal class ChatMessageViewModel(val message: MessageInfoModel)
