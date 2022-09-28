af2a38a06c27045d5daea1847ab2afd94bc3890e
adamhammer <adamhammer@microsoft.com>
2022-09-27 17:35:06 -0700

    comments

--- azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/presentation/ui/view_model/ChatScreenViewModel.kt
+++ azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/presentation/ui/view_model/ChatScreenViewModel.kt
@@ -7,6 +7,7 @@ import com.azure.android.communication.ui.chat.redux.state.ReduxState
 // View Model for the Chat Screen
 data class ChatScreenViewModel(val messages:List<ChatMessage>, val state: String, var buildCount: Int)
 
+// Internal counter for early debugging
 private var buildCount = 0
 
 // Methods to Build the Chat Screen View Model from the Store
