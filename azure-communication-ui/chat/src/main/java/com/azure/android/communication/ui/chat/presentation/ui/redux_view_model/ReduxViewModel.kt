a392622a71e519a1b73d0284d8e61d5c057b19f7
adamhammer <adamhammer@microsoft.com>
2022-09-28 10:44:35 -0700

    disabled test for now

--- azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/presentation/ui/redux_view_model/ReduxViewModel.kt
+++ azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/presentation/ui/redux_view_model/ReduxViewModel.kt
@@ -1,18 +1,33 @@
 package com.azure.android.communication.ui.chat.presentation.ui.redux_view_model
 
-import androidx.lifecycle.LifecycleCoroutineScope
 import com.azure.android.communication.ui.chat.redux.AppStore
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
+/**
+ * Redux View Model
+ *
+ * Listens to the Store and Emits ViewModels
+ *
+ *  T = Type of the Store store
+ *  M = Type of the ViewModel
+
+ * Params:
+ *   builder: (T) -> M
+ *   onChanged: (M) -> Unit
+ *   coroutineScope: The lifecycle scope to run this under
+ *   store: The Store you want to listen to
+ */
 internal class ReduxViewModel<T, M : Any>(
     val builder: (store: AppStore<T>) -> M,
     val onChanged: (viewModel: M) -> Unit,
-    private val coroutineScope: LifecycleCoroutineScope,
+    private val coroutineScope: CoroutineScope,
     private val store: AppStore<T>
 ) {
     lateinit var viewModel: M
 
+    // Rebuild the View Model
     private fun rebuild(store: AppStore<T>) {
         val newState = builder(store)
 
@@ -26,6 +41,7 @@ internal class ReduxViewModel<T, M : Any>(
         }
     }
 
+    // Post the onChanged notification on the main thread
     private fun postOnChanged() {
         coroutineScope.launch(Dispatchers.Main) {
             onChanged(viewModel)
