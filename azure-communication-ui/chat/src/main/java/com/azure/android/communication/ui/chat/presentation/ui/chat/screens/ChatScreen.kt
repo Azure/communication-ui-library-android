9e6c90a4af175caa4a3fba2c03bb42f0467b452b
adamhammer <adamhammer@microsoft.com>
2022-09-30 12:21:47 -0700

    basic UI scaffolding

--- azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/presentation/ui/chat/screens/ChatScreen.kt
+++ azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/presentation/ui/chat/screens/ChatScreen.kt
@@ -3,16 +3,31 @@
 
 package com.azure.android.communication.ui.chat.presentation.ui.chat.screens
 
-import androidx.compose.foundation.text.BasicText
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.azure.android.communication.ui.chat.models.MessageInfoModel
 import com.azure.android.communication.ui.chat.presentation.style.ChatCompositeUITheme
+import com.azure.android.communication.ui.chat.presentation.ui.chat.components.ChatCompositeActionBar
+import com.azure.android.communication.ui.chat.presentation.ui.chat.components.ChatCompositeBottomBar
+import com.azure.android.communication.ui.chat.presentation.ui.chat.components.ChatCompositeMessageList
 import com.azure.android.communication.ui.chat.presentation.ui.viewmodel.ChatScreenViewModel
+import com.azure.android.communication.ui.chat.presentation.ui.viewmodel.toViewModelList
+import com.azure.android.communication.ui.chat.service.sdk.wrapper.ChatMessageType
 
 @Composable
 internal fun ChatScreen(viewModel: ChatScreenViewModel) {
-    BasicText(
-        text = "Hello Chat! ${viewModel.messages.size} ${viewModel.state} builds: ${viewModel.buildCount}",
+    Scaffold(
+        topBar = { ChatCompositeActionBar() },
+        content = {
+            ChatCompositeMessageList(
+                modifier = Modifier.padding(it),
+                messages = viewModel.messages
+            )
+        },
+        bottomBar = { ChatCompositeBottomBar() }
     )
 }
 
@@ -22,7 +37,29 @@ internal fun ChatScreenPreview() {
     ChatCompositeUITheme {
         ChatScreen(
             viewModel = ChatScreenViewModel(
-                listOf(),
+                listOf(
+                    MessageInfoModel(
+                        messageType = ChatMessageType.TEXT,
+                        content = "Test Message",
+                        internalId = null,
+                        id = null
+                    ),
+
+                    MessageInfoModel(
+                        messageType = ChatMessageType.TEXT,
+                        content = "Test Message 2 ",
+                        internalId = null,
+                        id = null
+                    ),
+
+                    MessageInfoModel(
+                        messageType = ChatMessageType.TEXT,
+                        content = "Test Message 3",
+                        internalId = null,
+                        id = null
+                    ),
+
+                ).toViewModelList(),
                 state = "state",
                 buildCount = 2,
                 postMessage = {}
