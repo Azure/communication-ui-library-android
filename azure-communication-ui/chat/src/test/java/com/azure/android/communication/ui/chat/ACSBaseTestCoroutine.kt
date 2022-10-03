f254e549265ed91a386a546923ee8d0327c684dd
Mrayyan <t-musiddiqui@microsoft.com>
2022-10-03 09:11:27 -0700

    [Chat][Feature] Chat middleware and action handler tests (#463)

--- azure-communication-ui/chat/src/test/java/com/azure/android/communication/ui/chat/ACSBaseTestCoroutine.kt
+++ azure-communication-ui/chat/src/test/java/com/azure/android/communication/ui/chat/ACSBaseTestCoroutine.kt
@@ -1,9 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-package com.azure.android.communication.ui
+package com.azure.android.communication.ui.chat
 
-import com.azure.android.communication.ui.helper.MainCoroutineRule
+import com.azure.android.communication.ui.chat.helper.MainCoroutineRule
 import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.runTest
 import org.junit.Rule
---
+++ azure-communication-ui/chat/src/test/java/com/azure/android/communication/ui/chat/redux/middleware/handler/ChatMiddlewareActionHandlerUnitTest.kt
@@ -0,0 +1,115 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.android.communication.ui.chat.redux.middleware.handler
+
+import com.azure.android.communication.ui.chat.ACSBaseTestCoroutine
+import com.azure.android.communication.ui.chat.models.MessageInfoModel
+import com.azure.android.communication.ui.chat.redux.AppStore
+import com.azure.android.communication.ui.chat.redux.action.ChatAction
+import com.azure.android.communication.ui.chat.redux.action.ErrorAction
+import com.azure.android.communication.ui.chat.redux.middleware.sdk.ChatActionHandler
+import com.azure.android.communication.ui.chat.redux.state.ReduxState
+import com.azure.android.communication.ui.chat.service.ChatService
+import com.azure.android.communication.ui.chat.service.sdk.wrapper.ChatMessageType
+import com.azure.android.communication.ui.chat.service.sdk.wrapper.SendChatMessageResult
+import java9.util.concurrent.CompletableFuture
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.doAnswer
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.any
+import org.mockito.kotlin.argThat
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.times
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+internal class ChatMiddlewareActionHandlerUnitTest : ACSBaseTestCoroutine() {
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun chatMiddlewareActionHandler_sendMessage_then_dispatch_ChatActionSentMessage() =
+        runScopedTest {
+            // arrange
+            val messageInfoModel = MessageInfoModel(
+                id = null,
+                internalId = "54321",
+                messageType = ChatMessageType.TEXT,
+                content = "hello, world!"
+            )
+
+            val returnMessageId = "test"
+
+            val sendChatMessageResult = SendChatMessageResult(returnMessageId)
+
+            val sendChatMessageCompletableFuture = CompletableFuture<SendChatMessageResult>()
+
+            val mockChatService: ChatService = mock {
+                on { sendMessage(messageInfoModel) } doReturn sendChatMessageCompletableFuture
+            }
+
+            val chatHandler = ChatActionHandler(mockChatService)
+
+            val action = ChatAction.SendMessage(messageInfoModel)
+
+            val mockAppStore = mock<AppStore<ReduxState>> {
+                on { dispatch(any()) } doAnswer { }
+            }
+
+            // act
+            chatHandler.onAction(action, mockAppStore::dispatch)
+
+            sendChatMessageCompletableFuture.complete(sendChatMessageResult)
+
+            // assert
+            verify(mockAppStore, times(1)).dispatch(
+                argThat { action ->
+                    action is ChatAction.MessageSent && action.messageInfoModel.id == returnMessageId
+                }
+            )
+        }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun chatMiddlewareActionHandler_sendMessage_then_dispatch_ChatStateErrorOccurred() =
+        runScopedTest {
+            // arrange
+            val messageInfoModel = MessageInfoModel(
+                id = null,
+                internalId = "54321",
+                messageType = ChatMessageType.TEXT,
+                content = "hello, world!"
+            )
+
+            val error = Exception("test")
+
+            val sendChatMessageCompletableFuture = CompletableFuture<SendChatMessageResult>()
+
+            val mockChatService: ChatService = mock {
+                on { sendMessage(messageInfoModel) } doReturn sendChatMessageCompletableFuture
+            }
+
+            val chatHandler = ChatActionHandler(mockChatService)
+
+            val action = ChatAction.SendMessage(messageInfoModel)
+
+            val mockAppStore = mock<AppStore<ReduxState>> {
+                on { dispatch(any()) } doAnswer { }
+            }
+
+            // act
+            chatHandler.onAction(action, mockAppStore::dispatch)
+
+            sendChatMessageCompletableFuture.completeExceptionally(error)
+
+            // assert
+            verify(mockAppStore, times(1)).dispatch(
+                argThat { action ->
+                    action is ErrorAction.ChatStateErrorOccurred
+                }
+            )
+        }
+}
