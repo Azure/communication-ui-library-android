d7b270ff17200d1c4591cd1aec2432e6c7217587
Peixin Liu <1281563+lpx1989@users.noreply.github.com>
2022-10-04 11:17:33 -0700

    Add send message button (#472)
    Co-authored-by: Peixin Liu <peixinliu@Peixins-MacBook-Pro.local>

--- azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/presentation/ui/chat/components/ChatCompositeBottomBar.kt
+++ azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/presentation/ui/chat/components/ChatCompositeBottomBar.kt
@@ -4,11 +4,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -16,9 +14,7 @@ internal fun ChatCompositeBottomBar() {
     Box(modifier = Modifier.fillMaxWidth()) {
         Row {
             TextField(value = "", onValueChange = {}, modifier = Modifier.fillMaxWidth(fraction = 0.8f))
-            ClickableText(text = AnnotatedString("Send"), onClick = {
-                // Send Message
-            })
+            AcsChatSendMessageButton("Send Message Button")
         }
     }
 }
