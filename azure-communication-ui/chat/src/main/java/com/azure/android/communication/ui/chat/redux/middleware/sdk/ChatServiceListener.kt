aeddab737476d1783ab6855df89cc0020f6855e7
Inderpal Singh Aulakh <73618019+iaulakh@users.noreply.github.com>
2022-10-04 09:26:30 -0700

    refactoring

--- azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/redux/middleware/sdk/ChatServiceListener.kt
+++ azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/redux/middleware/sdk/ChatServiceListener.kt
@@ -5,9 +5,10 @@ package com.azure.android.communication.ui.chat.redux.middleware.sdk
 
 import com.azure.android.communication.ui.chat.error.ChatStateError
 import com.azure.android.communication.ui.chat.error.ErrorCode
+import com.azure.android.communication.ui.chat.models.ChatEventModel
+import com.azure.android.communication.ui.chat.models.MessagesPageModel
 import com.azure.android.communication.ui.chat.models.ChatThreadInfoModel
 import com.azure.android.communication.ui.chat.models.MessageInfoModel
-import com.azure.android.communication.ui.chat.models.MessagesPageModel
 import com.azure.android.communication.ui.chat.models.ParticipantTimestampInfoModel
 import com.azure.android.communication.ui.chat.models.RemoteParticipantsInfoModel
 import com.azure.android.communication.ui.chat.redux.Dispatch
@@ -48,36 +49,7 @@ internal class ChatServiceListener(
 
         coroutineScope.launch {
             chatService.getChatEventSharedFlow().collect {
-                when (it.eventType) {
-                    ChatEventType.CHAT_MESSAGE_RECEIVED -> {
-                        val model = it.infoModel as MessageInfoModel
-                    }
-                    ChatEventType.CHAT_MESSAGE_EDITED -> {
-                        val model = it.infoModel as MessageInfoModel
-                    }
-                    ChatEventType.CHAT_MESSAGE_DELETED -> {
-                        val model = it.infoModel as MessageInfoModel
-                    }
-                    ChatEventType.TYPING_INDICATOR_RECEIVED -> {
-                        val model = it.infoModel as ParticipantTimestampInfoModel
-                    }
-                    ChatEventType.READ_RECEIPT_RECEIVED -> {
-                        val model = it.infoModel as ParticipantTimestampInfoModel
-                    }
-                    ChatEventType.CHAT_THREAD_DELETED -> {
-                        val model = it.infoModel as ChatThreadInfoModel
-                    }
-                    ChatEventType.CHAT_THREAD_PROPERTIES_UPDATED -> {
-                        val model = it.infoModel as ChatThreadInfoModel
-                    }
-                    ChatEventType.PARTICIPANTS_ADDED -> {
-                        val model = it.infoModel as RemoteParticipantsInfoModel
-                    }
-                    ChatEventType.PARTICIPANTS_REMOVED -> {
-                        val model = it.infoModel as RemoteParticipantsInfoModel
-                    }
-                    else -> {}
-                }
+                handleInfoModel(it)
             }
         }
     }
@@ -104,6 +76,58 @@ internal class ChatServiceListener(
 
         if (messagesPageModel.allPagesFetched) {
             dispatch(ChatAction.AllMessagesFetched())
+        }
+    }
+
+    private fun handleInfoModel(it: ChatEventModel) {
+        when (it.infoModel) {
+            is MessageInfoModel -> {
+                when (it.eventType) {
+                    ChatEventType.CHAT_MESSAGE_RECEIVED -> {
+                        val model = it.infoModel
+                    }
+                    ChatEventType.CHAT_MESSAGE_EDITED -> {
+                        val model = it.infoModel
+                    }
+                    ChatEventType.CHAT_MESSAGE_DELETED -> {
+                        val model = it.infoModel
+                    }
+                    else -> {}
+                }
+            }
+            is ParticipantTimestampInfoModel -> {
+                when (it.eventType) {
+                    ChatEventType.TYPING_INDICATOR_RECEIVED -> {
+                        val model = it.infoModel
+                    }
+                    ChatEventType.READ_RECEIPT_RECEIVED -> {
+                        val model = it.infoModel
+                    }
+                    else -> {}
+                }
+            }
+            is ChatThreadInfoModel -> {
+                when (it.eventType) {
+                    ChatEventType.CHAT_THREAD_DELETED -> {
+                        val model = it
+                    }
+                    ChatEventType.CHAT_THREAD_PROPERTIES_UPDATED -> {
+                        val model = it
+                    }
+                    else -> {}
+                }
+            }
+            is RemoteParticipantsInfoModel -> {
+                when (it.eventType) {
+                    ChatEventType.PARTICIPANTS_ADDED -> {
+                        val model = it
+                    }
+                    ChatEventType.PARTICIPANTS_REMOVED -> {
+                        val model = it
+                    }
+                    else -> {}
+                }
+            }
         }
     }
 }
