a2f28c1d49dc4814a8f862708ab82c88e9693489
Mohtasim <mbellah@microsoft.com>
2022-10-06 12:36:31 -0700

    RepositoryMiddleware unit tests
    Signed-off-by: Mohtasim <mbellah@microsoft.com>

---
+++ azure-communication-ui/chat/src/test/java/com/azure/android/communication/ui/chat/redux/middleware/repository/RepositoryMiddlewareUnitTest.kt
@@ -0,0 +1,54 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.android.communication.ui.chat.redux.middleware.repository
+
+import com.azure.android.communication.ui.chat.models.MessageInfoModel
+import com.azure.android.communication.ui.chat.redux.AppStore
+import com.azure.android.communication.ui.chat.redux.action.ChatAction
+import com.azure.android.communication.ui.chat.redux.state.ReduxState
+import com.azure.android.communication.ui.chat.repository.MessageRepositoryMiddlewareInterface
+import com.azure.android.communication.ui.chat.service.sdk.wrapper.ChatMessageType
+import org.junit.Assert
+import org.junit.Test
+import org.mockito.kotlin.mock
+
+internal class RepositoryMiddlewareUnitTest {
+
+    @Test
+    fun messageRepositoryMiddleware_invoke_when_invokedWithAnyAction_then_invokeNext() {
+
+        val message = MessageInfoModel(
+            id = "1",
+            content = "Message 1",
+            messageType = ChatMessageType.TEXT
+        )
+
+        // arrange
+        val actionToDispatch = ChatAction.SendMessage(message)
+        var nextReceivedAction: ChatAction? = null
+
+        val mockMessageRepository = mock<MessageRepositoryMiddlewareInterface> {}
+
+        val messageRepositoryMiddlewareImplementation =
+            RepositoryMiddlewareImpl(
+                mockMessageRepository
+            )
+
+        val mockAppStore = mock<AppStore<ReduxState>> {}
+
+        // act
+        messageRepositoryMiddlewareImplementation.invoke(mockAppStore)(
+            fun(action) {
+                nextReceivedAction = action as ChatAction
+            }
+        )(actionToDispatch)
+
+        // assert
+        Assert.assertEquals(
+            actionToDispatch,
+            nextReceivedAction
+        )
+    }
+
+}
