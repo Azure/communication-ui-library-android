53540cc1fe545efe5bc37911661b55306e448e08
Mohtasim <mbellah@microsoft.com>
2022-10-06 11:38:00 -0700

    Writing unit test for bulk meesage insert
    Signed-off-by: Mohtasim <mbellah@microsoft.com>

---
+++ azure-communication-ui/chat/src/test/java/com/azure/android/communication/ui/chat/repository/MessageRepositoryUnitTest.kt
@@ -0,0 +1,44 @@
+/*
+ * *
+ *  * Copyright (c) Microsoft Corporation. All rights reserved.
+ *  * Licensed under the MIT License.
+ *
+ */
+
+package com.azure.android.communication.ui.chat.repository
+
+import com.azure.android.communication.ui.chat.models.MessageInfoModel
+import com.azure.android.communication.ui.chat.service.sdk.wrapper.ChatMessageType
+import org.junit.Assert
+import org.junit.Test
+
+
+internal class MessageRepositoryUnitTest {
+
+    @Test
+    fun messageRepository_addBulkRemoteMessage() {
+
+        val messageRepository: MessageRepositoryMiddlewareInterface = MessageRepository()
+
+        val messages = mutableListOf<MessageInfoModel>()
+        val numberOfTestMessages = 51
+        for (i in 0..50) {
+            messages.add(
+                MessageInfoModel(
+                    id = i.toString(),
+                    content = "Message $i",
+                    messageType = ChatMessageType.TEXT
+                )
+            )
+        }
+
+        messageRepository.addBulkRemoteMessage(messages)
+
+        Assert.assertEquals(numberOfTestMessages, messageRepository.size)
+
+        for (i in 0..50) {
+            Assert.assertEquals("Message $i", messageRepository.get(i).content)
+        }
+
+    }
+}
