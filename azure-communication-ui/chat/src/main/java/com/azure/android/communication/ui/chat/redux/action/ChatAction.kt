f6cd71796803f98babf2b7f14dfdc67a662a2dd3
Peixin Liu <peixinliu@microsoft.com>
2022-10-14 09:35:24 -0700

    [Chat][Feature] Add Redux state for send typing indicator and read receipt (#493)

--- azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/redux/action/ChatAction.kt
+++ azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/redux/action/ChatAction.kt
@@ -21,4 +21,6 @@ internal sealed class ChatAction : Action {
     class EndChat : ChatAction()
     class MessageReceived(val message: MessageInfoModel) : ChatAction()
     class MessageEdited(val message: MessageInfoModel) : ChatAction()
+    class MessageRead(val messageId: String) : ChatAction()
+    class TypingIndicator : ChatAction()
 }
