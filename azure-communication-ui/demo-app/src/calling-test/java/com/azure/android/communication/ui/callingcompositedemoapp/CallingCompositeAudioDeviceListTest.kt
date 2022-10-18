a732ddf682f7507df8f0b9126d461fcfe5bb1395
Mohtasim <mbellah@microsoft.com>
2022-10-18 14:16:04 -0700

    [Feature][Calling] audio device selection expresso test
    Signed-off-by: Mohtasim <mbellah@microsoft.com>

--- azure-communication-ui/demo-app/src/calling-test/java/com/azure/android/communication/ui/callingcompositedemoapp/CallingCompositeAudioDeviceListTest.kt
+++ azure-communication-ui/demo-app/src/calling-test/java/com/azure/android/communication/ui/callingcompositedemoapp/CallingCompositeAudioDeviceListTest.kt
@@ -26,7 +26,7 @@ class CallingCompositeAudioDeviceListTest : BaseUiTest() {
     @Test
     fun selectSpeakerAudioDevice() {
         joinGroupSetupScreen()
-            .selectSpeakerAudioDevice(false)
+            .selectSpeakerAudioDevice(true)
             .verifyIsSpeakerAudioDevice()
             .navigateUpFromSetupScreen()
     }
