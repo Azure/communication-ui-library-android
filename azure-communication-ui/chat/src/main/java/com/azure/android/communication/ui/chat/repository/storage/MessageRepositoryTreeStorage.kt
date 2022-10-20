36a9517395de44d2efee797a566ea10f6806e400
Mohtasim <mbellah@microsoft.com>
2022-10-19 17:56:02 -0700

    Storage Implementation with Tree based data structure
    Signed-off-by: Mohtasim <mbellah@microsoft.com>

---
+++ azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/repository/storage/MessageRepositoryTreeStorage.kt
@@ -0,0 +1,112 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.android.communication.ui.chat.repository.storage
+
+import com.azure.android.communication.ui.chat.models.MessageInfoModel
+import com.azure.android.communication.ui.chat.repository.MessageRepositoryReader
+import com.azure.android.communication.ui.chat.repository.MessageRepositoryWriter
+import java.util.TreeMap
+
+internal class MessageRepositoryTreeWriter: MessageRepositoryWriter {
+
+    private val treeMapStoragePointer: TreeMap<Long, String> = TreeMap()
+    private val treeMapStorage: TreeMap<String, MessageInfoModel> = TreeMap()
+
+    val size: Int
+        get() = treeMapStorage.size
+
+    override fun addLocalMessage(messageInfoModel: MessageInfoModel) {
+        val orderId: Long = getOrderId(messageInfoModel)
+
+        messageInfoModel.id?.let { treeMapStoragePointer.put(orderId, it) }
+        messageInfoModel.id?.let { treeMapStorage.put(it, messageInfoModel) }
+    }
+
+    override fun addPage(page: List<MessageInfoModel>) {
+        page.forEach { it -> addLocalMessage(it) }
+    }
+
+    override fun addServerMessage(message: MessageInfoModel) {
+        addLocalMessage(message)
+    }
+
+    override fun removeMessage(message: MessageInfoModel) {
+        val orderId = getOrderId(message)
+
+        if (treeMapStoragePointer.contains(orderId)) {
+            treeMapStoragePointer.remove(orderId)
+            treeMapStorage.remove(message.id)
+        }
+    }
+
+    override fun editMessage(message: MessageInfoModel) {
+        val orderId = getOrderId(message)
+
+        if (treeMapStoragePointer.contains(orderId)) {
+            treeMapStorage.get(message.id)?.let { mergeWithPreviousMessage(it, message) }
+        } else {
+            addLocalMessage(message)
+        }
+    }
+
+    override fun getLastMessage(): MessageInfoModel? {
+        return treeMapStorage.lastEntry().value
+    }
+
+    fun searchItem(kth: Int): MessageInfoModel {
+
+        var highestKey = treeMapStoragePointer.lastKey()
+        var lowestKey = treeMapStoragePointer.firstKey()
+        var elements = 0
+        var midKey: Long = 0
+        while(lowestKey < highestKey) {
+            midKey = (highestKey + lowestKey).div(2)
+
+            elements = treeMapStoragePointer.headMap(midKey).size
+
+            if(elements < kth) {
+                lowestKey = midKey + 1
+            } else if (elements > kth) {
+                highestKey = midKey - 1
+            } else {
+                break
+            }
+        }
+
+        val key = treeMapStoragePointer.headMap(midKey).lastKey()
+        return treeMapStorage.get(treeMapStoragePointer.get(key))!!
+    }
+
+    private fun getOrderId(message: MessageInfoModel): Long {
+        return message.createdOn?.toEpochSecond() ?: 0
+    }
+
+    private fun mergeWithPreviousMessage(previousMessage: MessageInfoModel,
+                                         message: MessageInfoModel): MessageInfoModel {
+        var newMessage = MessageInfoModel(
+            id = previousMessage.id,
+            internalId = previousMessage.internalId,
+            content = message.content,
+            messageType = previousMessage.messageType,
+            version = previousMessage.version,
+            senderDisplayName = previousMessage.senderDisplayName,
+            createdOn = previousMessage.createdOn,
+            editedOn = previousMessage.editedOn,
+            deletedOn = previousMessage.deletedOn,
+            senderCommunicationIdentifier = previousMessage.senderCommunicationIdentifier
+        )
+        return newMessage
+    }
+
+}
+
+internal class MessageRepositoryTreeReader(private val writer: MessageRepositoryTreeWriter): MessageRepositoryReader() {
+
+    override val size: Int
+        get() = writer.size
+
+    override fun get(index: Int): MessageInfoModel {
+        return writer.searchItem(index+1)
+    }
+}
