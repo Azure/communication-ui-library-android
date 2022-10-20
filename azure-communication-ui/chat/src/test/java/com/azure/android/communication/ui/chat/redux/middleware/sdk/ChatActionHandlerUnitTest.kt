f63029a28dd86898bb93c1cbd1514dc4d9387dd1
Inderpal Singh Aulakh <73618019+iaulakh@users.noreply.github.com>
2022-10-19 22:36:07 -0700

    Update ChatActionHandlerUnitTest.kt

--- azure-communication-ui/chat/src/test/java/com/azure/android/communication/ui/chat/redux/middleware/sdk/ChatActionHandlerUnitTest.kt
+++ azure-communication-ui/chat/src/test/java/com/azure/android/communication/ui/chat/redux/middleware/sdk/ChatActionHandlerUnitTest.kt
@@ -21,6 +21,8 @@ import com.azure.android.communication.ui.chat.service.sdk.wrapper.ChatMessageTy
 import com.azure.android.communication.ui.chat.service.sdk.wrapper.SendChatMessageResult
 import java9.util.concurrent.CompletableFuture
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -640,5 +642,36 @@ internal class ChatActionHandlerUnitTest : ACSBaseTestCoroutine() {
 
             // assert
             verify(mockChatService, times(1)).sendTypingIndicator()
+        }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun chatMiddlewareActionHandler_on_actionsTypingIndicatorAfterInterval_then_sendTypingIndicatorTwice() =
+        runScopedTest {
+            runBlocking {
+                // arrange
+
+                val sendTypingIndicatorCompletableFuture = CompletableFuture<Void>()
+
+                val mockChatService: ChatService = mock {
+                    on { sendTypingIndicator() } doReturn sendTypingIndicatorCompletableFuture
+                }
+
+                val chatHandler = ChatActionHandler(mockChatService)
+
+                val action = ChatAction.TypingIndicator()
+
+                val mockAppStore = mock<AppStore<ReduxState>> { }
+                val mockAppState = mock<ReduxState> {}
+
+                // act
+                chatHandler.onAction(action, mockAppStore::dispatch, mockAppState)
+                delay(ChatActionHandler.SEND_TYPING_INDICATOR_INTERVAL_MILLIS.toLong())
+                chatHandler.onAction(action, mockAppStore::dispatch, mockAppState)
+                sendTypingIndicatorCompletableFuture.complete(null)
+
+                // assert
+                verify(mockChatService, times(2)).sendTypingIndicator()
+            }
         }
 }
