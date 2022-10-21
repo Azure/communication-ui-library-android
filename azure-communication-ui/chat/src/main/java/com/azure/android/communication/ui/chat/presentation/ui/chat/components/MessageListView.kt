40973b01a28f746585afd200b80a05945ffd178a
adamhammer <adamhammer@microsoft.com>
2022-10-21 01:17:04 -0700

    auto-paging instead of auto-scroll

--- azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/presentation/ui/chat/components/MessageListView.kt
+++ azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/presentation/ui/chat/components/MessageListView.kt
@@ -8,13 +8,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.LaunchedEffect
-
 
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,38 +19,57 @@ import com.azure.android.communication.ui.chat.presentation.ui.viewmodel.Message
 import com.azure.android.communication.ui.chat.presentation.ui.viewmodel.toViewModelList
 import com.azure.android.communication.ui.chat.preview.MOCK_LOCAL_USER_ID
 import com.azure.android.communication.ui.chat.preview.MOCK_MESSAGES
-import com.azure.android.communication.ui.chat.utilities.isScrolledToEnd
+import com.azure.android.communication.ui.chat.redux.Dispatch
+import com.azure.android.communication.ui.chat.redux.action.ChatAction
 import com.azure.android.communication.ui.chat.utilities.outOfViewItemCount
-import kotlinx.coroutines.launch
+
+const val MESSAGE_LIST_LOAD_MORE_THRESHOLD = 40
 
 @Composable
 internal fun MessageListView(
     modifier: Modifier,
     messages: List<MessageViewModel>,
     scrollState: LazyListState,
+    dispatchers: Dispatch
 ) {
 
-    val autoScroll = remember { mutableStateOf(true) }
-    val scope = rememberCoroutineScope()
-
-    if (scrollState.isScrollInProgress) {
-        autoScroll.value = scrollState.isScrolledToEnd()
-    }
-
-    if (scrollState.outOfViewItemCount() > 0 && autoScroll.value) {
-        LaunchedEffect(scrollState.outOfViewItemCount()) {
-            scope.launch {
-                scrollState.animateScrollToItem(messages.size - 1)
-            }
-        }
-    }
+    requestPages(scrollState, messages, dispatchers)
+    // autoScroll(scrollState, messages)
 
     LazyColumn(
         modifier = modifier.fillMaxHeight(),
-        state = scrollState
+        state = scrollState,
+        reverseLayout = true,
     ) {
-        items(messages) { message ->
+        items(messages.asReversed()) { message ->
             MessageView(message)
+        }
+    }
+}
+
+// Handle paging request
+//
+// If there is < Threshold out of screen
+// we request more pages and mark a bookmark on the oldest message
+// The bookmark blocks paging requests until it's no longer the oldest message
+// i.e.
+// If bookmark is at the end, no paging
+// if bookmark is in middle, and out of screen < Threshold, page more data
+@Composable
+private fun requestPages(
+    scrollState: LazyListState,
+    messages: List<MessageViewModel>,
+    dispatch: Dispatch
+) {
+    if (scrollState.layoutInfo.totalItemsCount == 0) return
+
+    val currentLastMessage = messages.first()
+
+    if (scrollState.outOfViewItemCount() < MESSAGE_LIST_LOAD_MORE_THRESHOLD) {
+        val lastTrigger = remember { mutableStateOf("0") }
+        if (lastTrigger.value != currentLastMessage.message.id) {
+            lastTrigger.value = currentLastMessage.message.id ?: "0"
+            dispatch(ChatAction.FetchMessages())
         }
     }
 }
@@ -66,5 +81,5 @@ internal fun PreviewMessageListView() {
         modifier = Modifier.padding(0.dp),
         messages = MOCK_MESSAGES.toViewModelList(MOCK_LOCAL_USER_ID),
         scrollState = LazyListState(),
-    )
+    ) {}
 }
