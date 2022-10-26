61fe7ac357ce901915f9c1f8abc8bfc3e3f2bfe9
adamhammer <adamhammer@microsoft.com>
2022-10-26 11:34:00 -0700

    copyright

--- azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/presentation/ui/chat/components/FluentCircularIndicator.kt
+++ azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/presentation/ui/chat/components/FluentCircularIndicator.kt
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.azure.android.communication.ui.chat.presentation.ui.chat.components
 
 import androidx.compose.runtime.Composable
