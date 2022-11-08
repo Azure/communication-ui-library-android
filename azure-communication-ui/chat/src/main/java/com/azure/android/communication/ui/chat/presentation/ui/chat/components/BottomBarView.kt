164c44d3543c4533423fa5522b2f8f13af50948b
ShaunaSong <93549644+ShaunaSong@users.noreply.github.com>
2022-11-08 12:18:26 -0800

    Update disable state

--- azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/presentation/ui/chat/components/BottomBarView.kt
+++ azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/presentation/ui/chat/components/BottomBarView.kt
@@ -45,7 +45,8 @@ internal fun BottomBarView(
 
         SendMessageButtonView(
             contentDescription = stringResource(R.string.azure_communication_ui_chat_message_send_button_content_description, messageInputTextState.value),
-            chatStatus = chatStatus
+            chatStatus = chatStatus,
+            clickable = messageInputTextState.value.isNotBlank()
         ) {
             sendButtonOnclick(postAction, messageInputTextState)
         }
@@ -53,7 +54,6 @@ internal fun BottomBarView(
 }
 
 private fun sendButtonOnclick(postAction: (Action) -> Unit, messageInputTextState: MutableState<String>) {
-    if (messageInputTextState.value.isBlank()) return
     postAction(
         ChatAction.SendMessage(
             MessageInfoModel(
