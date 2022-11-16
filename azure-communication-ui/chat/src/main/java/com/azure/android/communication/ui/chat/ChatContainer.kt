156eaa194475b3850347dcba4d7b6f48d0312dbb
Mohtasim <99507832+Mohtasim@users.noreply.github.com>
2022-11-15 16:10:42 -0800

    [Chat][Feature] Telemetry config implemented with Chatcomposite (#592)

--- azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/ChatContainer.kt
+++ azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/ChatContainer.kt
@@ -64,7 +64,7 @@ internal class ChatContainer(
                     endPointURL = remoteOptions.locator.endpointURL,
                     identity = remoteOptions.identity,
                     credential = remoteOptions.credential,
-                    applicationID = "azure_communication_ui", // TODO: modify while working on diagnostics config < 24
+                    applicationID = DiagnosticConfig().tag,
                     sdkName = "com.azure.android:azure-communication-chat",
                     sdkVersion = "2.0.0",
                     threadId = remoteOptions.locator.chatThreadId,
