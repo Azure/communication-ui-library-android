ae6e6167bbbab7f523f63fbb37cfb217024fa691
ShaunaSong <93549644+ShaunaSong@users.noreply.github.com>
2022-11-18 08:52:58 -0800

    format

--- azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/redux/reducer/ChatReducer.kt
+++ azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/redux/reducer/ChatReducer.kt
@@ -39,7 +39,7 @@ internal class ChatReducerImpl : ChatReducer {
             }
             is ChatAction.MessageLastReceived -> {
                 state.copy(
-                    lastReadMessageId = if (state.lastReadMessageId> action.messageId) state.lastReadMessageId
+                    lastReadMessageId = if (state.lastReadMessageId > action.messageId) state.lastReadMessageId
                     else action.messageId
                 )
             }
