7d04226de035267ff6f26abbc633e60a83fbfe53
Pavel Prystinka <pprystinka@microsoft.com>
2022-11-18 12:02:09 -0800

    add setter for ChatAdapter and constructor overloads

--- azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/presentation/ChatCompositeView.java
+++ azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/presentation/ChatCompositeView.java
@@ -4,6 +4,7 @@
 package com.azure.android.communication.ui.chat.presentation;
 
 import android.content.Context;
+import android.util.AttributeSet;
 import android.widget.FrameLayout;
 
 import com.azure.android.communication.ui.chat.ChatAdapter;
@@ -14,13 +15,26 @@ import com.azure.android.communication.ui.chat.presentation.ui.container.ChatCom
  */
 public final class ChatCompositeView extends FrameLayout {
 
-    /**
-     * Creates chat composite view
-     * @param context
-     * @param chatAdapter
-     */
+    public ChatCompositeView(final Context context) {
+        super(context);
+    }
+
     public ChatCompositeView(final Context context, final ChatAdapter chatAdapter) {
         super(context);
-        addView(new ChatCompositeViewImpl(context, chatAdapter));
+        setChatAdapter(chatAdapter);
+    }
+
+    public ChatCompositeView(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ChatCompositeView(final Context context, final AttributeSet attrs, final ChatAdapter chatAdapter) {
+        super(context, attrs);
+        setChatAdapter(chatAdapter);
+    }
+
+    public ChatCompositeView setChatAdapter(final ChatAdapter chatAdapter) {
+        addView(new ChatCompositeViewImpl(this.getContext(), chatAdapter));
+        return this;
     }
 }
