ddcc1c6689eea4a11cac72dcc0cf3a77060d62e0
Adam Hammer <adamhammer@microsoft.com>
2022-11-22 15:31:33 -0800

    fix up findMessageIdxById

--- azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/utilities/MessageInfoModelListExtensions.kt
+++ azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/utilities/MessageInfoModelListExtensions.kt
@@ -2,20 +2,30 @@ package com.azure.android.communication.ui.chat.utilities
 
 import com.azure.android.communication.ui.chat.models.MessageInfoModel
 
+// Returns an index of -1 if item can't be found
 internal fun List<MessageInfoModel>.findMessageIdxById(messageId: String): Int {
+    if (messageId.trim() == "") return -1
     var first = 0
     var last = size - 1
     var mid: Int = size / 2
     while (first <= last) {
         mid = (first + last) / 2
-        val midVal = this[mid].id!!.toInt()
-        if (midVal!!.toInt() < messageId.toInt()) {
+
+
+        while (this[mid].id == null) {
+            mid--
+            if (mid < first) {
+                throw IllegalArgumentException("Message with id $messageId not found")
+            }
+        }
+        val midVal = this[mid].id!!.toLong()
+        if (midVal < messageId.toLong()) {
             first = mid + 1
-        } else if (midVal > messageId.toInt()) {
+        } else if (midVal > messageId.toLong()) {
             last = mid - 1
         } else {
             return mid
         }
     }
-    throw IllegalArgumentException("Message with id $messageId not found")
+    return -1
 }
