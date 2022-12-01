bac2f6d0ef96609e5ac7d7892debefce4b05c381
ShaunaSong <93549644+ShaunaSong@users.noreply.github.com>
2022-12-01 14:48:15 -0800

    ktlint

--- azure-communication-ui/chat/src/androidTest/java/com/azure/android/communication/ui/chat/MessageViewTest.kt
+++ azure-communication-ui/chat/src/androidTest/java/com/azure/android/communication/ui/chat/MessageViewTest.kt
@@ -16,47 +16,47 @@ import org.junit.Test
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-internal class MessageViewTest : BaseUiTest(){
+internal class MessageViewTest : BaseUiTest() {
     @get:Rule
     val composeTestRule = createComposeRule()
-    
+
     @Test
     fun testOnMessageContentWithCurrentTime() = runTest {
         injectDependencies(testScheduler)
-        
+
         // launch composite
         chatSDK.setChatStatus(ChatStatus.INITIALIZED)
         launchChatComposite()
-        
+
         // send message
         val message = "hello"
         composeTestRule.onNodeWithTag(UITestTags.MESSAGE_INPUT_BOX).performTextInput(message)
         composeTestRule.onNodeWithTag(UITestTags.MESSAGE_SEND_BUTTON).performClick()
-        
+
         // current system time
         val formattedDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("h:mm a"))
-        
+
         // assert message is cleared after send
         composeTestRule.onNodeWithTag(UITestTags.MESSAGE_TIME_CONTENT).assert(hasText(formattedDate))
     }
-    
+
     @Test
     fun testOnMessageContentWithNoCurrentTime() = runTest {
         injectDependencies(testScheduler)
-        
+
         // launch composite
         chatSDK.setChatStatus(ChatStatus.INITIALIZED)
         launchChatComposite()
-        
+
         // send message
         val message = "hello"
         composeTestRule.onNodeWithTag(UITestTags.MESSAGE_INPUT_BOX).performTextInput(message)
         composeTestRule.onNodeWithTag(UITestTags.MESSAGE_SEND_BUTTON).performClick()
-    
+
         val secondMessage = "HELLO"
         composeTestRule.onNodeWithTag(UITestTags.MESSAGE_INPUT_BOX).performTextInput(secondMessage)
         composeTestRule.onNodeWithTag(UITestTags.MESSAGE_SEND_BUTTON).performClick()
-        
+
         // assert message is cleared after send
         composeTestRule.onAllNodesWithTag(UITestTags.MESSAGE_BASIC_CONTENT).assertAny(hasText(secondMessage))
     }
