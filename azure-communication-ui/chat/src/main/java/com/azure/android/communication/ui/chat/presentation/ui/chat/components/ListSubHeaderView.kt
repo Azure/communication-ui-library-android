47b227d6eca5ed6ebe9e60a72abe55235e7bc152
Mrayyan <t-musiddiqui@microsoft.com>
2023-01-03 10:35:42 -0800

    [Chat][Bug] "In chat" width text (#676)

--- azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/presentation/ui/chat/components/ListSubHeaderView.kt
+++ azure-communication-ui/chat/src/main/java/com/azure/android/communication/ui/chat/presentation/ui/chat/components/ListSubHeaderView.kt
@@ -5,15 +5,18 @@
 
 package com.azure.android.communication.ui.chat.presentation.ui.chat.components
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 
 @Composable
 internal fun ListSubHeaderView(
     text: String,
+    modifier: Modifier = Modifier,
 ) {
-    AndroidView(factory = { context ->
+    AndroidView(modifier = modifier.fillMaxWidth(), factory = { context ->
         com.microsoft.fluentui.listitem.ListSubHeaderView(context).apply {
             title = text
         }
