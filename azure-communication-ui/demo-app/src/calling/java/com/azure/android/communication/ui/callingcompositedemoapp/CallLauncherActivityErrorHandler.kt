c2a700aef566aa533b0fda0087222aaa1e52551a
Pavel Prystinka <pprystinka@microsoft.com>
2023-02-08 23:14:48 -0800

    fix android tests

--- azure-communication-ui/demo-app/src/calling/java/com/azure/android/communication/ui/callingcompositedemoapp/CallLauncherActivityErrorHandler.kt
+++ azure-communication-ui/demo-app/src/calling/java/com/azure/android/communication/ui/callingcompositedemoapp/CallLauncherActivityErrorHandler.kt
@@ -9,35 +9,40 @@ import com.azure.android.communication.ui.calling.CallComposite
 import com.azure.android.communication.ui.calling.CallCompositeEventHandler
 import com.azure.android.communication.ui.calling.models.CallCompositeErrorEvent
 import com.microsoft.appcenter.utils.HandlerUtils.runOnUiThread
+import java.lang.ref.WeakReference
 
 // Handles forwarding of error messages to the CallLauncherActivity
 //
 // CallLauncherActivity is loosely coupled and will detach the weak reference after disposed.
 class CallLauncherActivityErrorHandler(
-    private val context: Context,
+    context: Context,
     private val callComposite: CallComposite,
 ) :
     CallCompositeEventHandler<CallCompositeErrorEvent> {
 
+    private val activityWr: WeakReference<Context> = WeakReference(context)
+
     override fun handle(it: CallCompositeErrorEvent) {
 
-        val lastCallId = callComposite.getDebugInfo(context).callHistoryRecords
-            .lastOrNull()?.callIds?.lastOrNull()?.toString() ?: ""
+        activityWr.get()?.let { context ->
+            val lastCallId = callComposite.getDebugInfo(context).callHistoryRecords
+                .lastOrNull()?.callIds?.lastOrNull()?.toString() ?: ""
 
-        println("================= application is logging exception =================")
-        println("call id: $lastCallId")
-        println(it.cause)
-        println(it.errorCode)
+            println("================= application is logging exception =================")
+            println("call id: $lastCallId")
+            println(it.cause)
+            println(it.errorCode)
 
-        runOnUiThread {
-            val builder = AlertDialog.Builder(context).apply {
-                setMessage("${it.errorCode} ${it.cause?.message}. Call id: $lastCallId")
-                setTitle("Alert")
-                setPositiveButton("OK") { _, _ ->
+            runOnUiThread {
+                val builder = AlertDialog.Builder(context).apply {
+                    setMessage("${it.errorCode} ${it.cause?.message}. Call id: $lastCallId")
+                    setTitle("Alert")
+                    setPositiveButton("OK") { _, _ ->
+                    }
                 }
+                builder.show()
             }
-            builder.show()
+            println("====================================================================")
         }
-        println("====================================================================")
     }
 }
