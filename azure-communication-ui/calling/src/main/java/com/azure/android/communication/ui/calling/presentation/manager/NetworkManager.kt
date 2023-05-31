a401c16b6f0ba934e67a7d3b7648516a42aeac59
Mohtasim <mbellah@microsoft.com>
2023-05-30 19:28:40 -0700

    Removing unsued import
    Signed-off-by: Mohtasim <mbellah@microsoft.com>

--- azure-communication-ui/calling/src/main/java/com/azure/android/communication/ui/calling/presentation/manager/NetworkManager.kt
+++ azure-communication-ui/calling/src/main/java/com/azure/android/communication/ui/calling/presentation/manager/NetworkManager.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
-import androidx.core.content.ContextCompat.getSystemService
 
 internal class NetworkManager(private val context: Context) {
 
