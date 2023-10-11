bb21872f041b9c0b5884a8c6bd2b2a334d07dcc6
iaulakh <iaulakh@microsoft.com>
2023-10-11 10:02:49 -0700

    Revert "Delete CallingCompositeBaselineUiTest.kt"
    This reverts commit 9a96292abf07a5b74430133a21d4fd89dd1edf8e.

--- azure-communication-ui/demo-app/src/calling-test/java/com/azure/android/communication/ui/callingcompositedemoapp/CallingCompositeAudioDeviceListTest.kt
+++ azure-communication-ui/demo-app/src/calling-test/java/com/azure/android/communication/ui/callingcompositedemoapp/CallingCompositeAudioDeviceListTest.kt
@@ -8,29 +8,30 @@ import androidx.test.filters.LargeTest
 import com.azure.android.communication.ui.callingcompositedemoapp.robots.HomeScreenRobot
 import com.azure.android.communication.ui.callingcompositedemoapp.robots.SetupScreenRobot
 import com.azure.android.communication.ui.callingcompositedemoapp.util.CallIdentifiersHelper
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
 class CallingCompositeAudioDeviceListTest : BaseUiTest() {
 
-//    @Test
-//    fun selectDefaultAudioDevice() {
-//        joinGroupSetupScreen()
-//            .tapSpeakerIcon()
-//            .selectSpeakerAudioDevice(true)
-//            .verifyIsSpeakerAudioDevice()
-//            .navigateUpFromSetupScreen()
-//    }
+    @Test
+    fun selectDefaultAudioDevice() {
+        joinGroupSetupScreen()
+            .tapSpeakerIcon()
+            .selectSpeakerAudioDevice(true)
+            .verifyIsSpeakerAudioDevice()
+            .navigateUpFromSetupScreen()
+    }
 
-//    @Test
-//    fun selectAndroidAudioDevice() {
-//        joinGroupSetupScreen()
-//            .tapSpeakerIcon()
-//            .selectAndroidAudioDevice(false)
-//            .verifyIsAndroidAudioDevice()
-//            .navigateUpFromSetupScreen()
-//    }
+    @Test
+    fun selectAndroidAudioDevice() {
+        joinGroupSetupScreen()
+            .tapSpeakerIcon()
+            .selectAndroidAudioDevice(false)
+            .verifyIsAndroidAudioDevice()
+            .navigateUpFromSetupScreen()
+    }
 
     private fun joinGroupSetupScreen(): SetupScreenRobot {
         val setupScreen = HomeScreenRobot()
