0b4f7ae11eab45a09a68c934226714875d51140d
Pratima Gauns <pgauns@microsoft.com>
2023-10-19 11:56:43 +0530

    Removing display name CallCompositeStartCallOptions

--- azure-communication-ui/calling/src/main/java/com/azure/android/communication/ui/calling/models/CallCompositeStartCallOptions.java
+++ azure-communication-ui/calling/src/main/java/com/azure/android/communication/ui/calling/models/CallCompositeStartCallOptions.java
@@ -1,18 +1,15 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 package com.azure.android.communication.ui.calling.models;
 
 import java.util.List;
 
 public class CallCompositeStartCallOptions {
-    private final String displayName;
     private final List<String> participants;
 
-    public CallCompositeStartCallOptions(String displayName, List<String> participants) {
-        this.displayName = displayName;
+    public CallCompositeStartCallOptions(final List<String> participants) {
         this.participants = participants;
-    }
-
-    public String getDisplayName() {
-        return displayName;
     }
 
     public List<String> getParticipants() {
