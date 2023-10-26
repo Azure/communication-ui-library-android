2234e78a72b3d706f95b020dbf54f2c081734f66
Pratima Gauns <pgauns@microsoft.com>
2023-10-26 15:55:01 +0530

    Handling null diContainer

--- azure-communication-ui/calling/src/main/java/com/azure/android/communication/ui/calling/handlers/PushNotificationHandler.kt
+++ azure-communication-ui/calling/src/main/java/com/azure/android/communication/ui/calling/handlers/PushNotificationHandler.kt
@@ -43,7 +43,6 @@ internal class PushNotificationHandler(private val callingSDK: CallingSDK? = nul
                             result.complete(t)
                         }
                     }
-                callAgent?.dispose()
             }
     }
 
