b7c72b086db9adbc1e899a71ce7363b2d2449072
Pratima Gauns <pgauns@microsoft.com>
2023-11-02 16:54:42 +0530

    Added Java Docs.

--- azure-communication-ui/calling/src/main/java/com/azure/android/communication/ui/calling/models/CallCompositePushNotificationInfo.java
+++ azure-communication-ui/calling/src/main/java/com/azure/android/communication/ui/calling/models/CallCompositePushNotificationInfo.java
@@ -5,17 +5,33 @@ package com.azure.android.communication.ui.calling.models;
 
 import java.util.Map;
 
+/**
+ * CallCompositePushNotificationInfo for forwarding calling push notifications to UI.
+ */
 public class CallCompositePushNotificationInfo {
     private final Map<String, String> notificationInfo;
 
+    /**
+     * Create {@link CallCompositePushNotificationInfo}.
+     * @param notificationInfo Notification info map.
+     */
     public CallCompositePushNotificationInfo(final Map<String, String> notificationInfo) {
         this.notificationInfo = notificationInfo;
     }
 
+    /**
+     * Get notification info map.
+     * @return {@link Map}.
+     */
     public Map<String, String> getNotificationInfo() {
         return notificationInfo;
     }
 
+    /**
+     * Create {@link CallCompositePushNotificationInfo} from map.
+     * @param map Notification info map.
+     * @return {@link CallCompositePushNotificationInfo}.
+     */
     public static CallCompositePushNotificationInfo fromMap(final Map<String, String> map) {
         return new CallCompositePushNotificationInfo(map);
     }
