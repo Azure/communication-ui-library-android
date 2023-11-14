9a1ce72cf6ed90b8bf99f7ceb4076f2b4da99cc9
Pratima Gauns <pgauns@microsoft.com>
2023-11-14 15:45:40 +0530

    Changes to wake up screen.

--- azure-communication-ui/demo-app/src/calling/java/com/azure/android/communication/ui/callingcompositedemoapp/DemoFirebaseMessagingService.kt
+++ azure-communication-ui/demo-app/src/calling/java/com/azure/android/communication/ui/callingcompositedemoapp/DemoFirebaseMessagingService.kt
@@ -3,7 +3,6 @@
 
 package com.azure.android.communication.ui.callingcompositedemoapp
 
-import android.app.ActivityManager
 import android.content.Context
 import android.media.Ringtone
 import android.os.Build
@@ -42,9 +41,7 @@ class DemoFirebaseMessagingService : FirebaseMessagingService() {
                     pushNotificationInfo.eventType.toString() + " handleIncomingCall"
                 )
 
-                if (!isForeground(applicationContext.packageName)) {
-                    wakeApp()
-                }
+                wakeAppIfScreenOff()
                 CallCompositeManager.getInstance().handleIncomingCall(
                     remoteMessage.data,
                     BuildConfig.ACS_TOKEN,
@@ -54,34 +51,22 @@ class DemoFirebaseMessagingService : FirebaseMessagingService() {
         }
     }
 
-    private fun wakeApp() {
+    private fun wakeAppIfScreenOff() {
         val pm = applicationContext.getSystemService(Context.POWER_SERVICE) as PowerManager
         val screenIsOn = pm.isInteractive // check if screen is on
 
         if (!screenIsOn) {
             val wakeLockTag: String = applicationContext.packageName + "WAKELOCK"
             val wakeLock = pm.newWakeLock(
-                PowerManager.ON_AFTER_RELEASE, wakeLockTag
+                PowerManager.FULL_WAKE_LOCK or
+                        PowerManager.ACQUIRE_CAUSES_WAKEUP or
+                        PowerManager.ON_AFTER_RELEASE, wakeLockTag
             )
 
             // acquire will turn on the display
             wakeLock.acquire(10 * 60 * 1000L /*10 minutes*/)
 
-            // release will release the lock from CPU, in case of that, screen will go back to sleep mode in defined time bt device settings
             wakeLock.release()
-        }
-    }
-
-    private fun isForeground(myPackage: String): Boolean {
-        val manager = applicationContext.getSystemService(ACTIVITY_SERVICE) as ActivityManager?
-        val runningTaskInfo = manager?.getRunningTasks(1)
-        return if (runningTaskInfo.isNullOrEmpty()) {
-            Log.d(CallLauncherActivity.TAG, "isForeground: runningTaskInfo is null or empty")
-            false
-        } else {
-            Log.d(CallLauncherActivity.TAG, "isForeground:" + runningTaskInfo[0].topActivity?.packageName)
-            val componentInfo = runningTaskInfo[0].topActivity
-            componentInfo!!.packageName == myPackage
         }
     }
 }
