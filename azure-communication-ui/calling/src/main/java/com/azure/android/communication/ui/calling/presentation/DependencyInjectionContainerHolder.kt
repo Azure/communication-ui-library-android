b212ca239d6990c56bf20665b664c80255a294d8
iaulakh <iaulakh@microsoft.com>
2023-12-11 21:01:54 -0800

    [Calling] check telecom manager flag for microphone check

--- azure-communication-ui/calling/src/main/java/com/azure/android/communication/ui/calling/presentation/DependencyInjectionContainerHolder.kt
+++ azure-communication-ui/calling/src/main/java/com/azure/android/communication/ui/calling/presentation/DependencyInjectionContainerHolder.kt
@@ -75,7 +75,8 @@ internal class DependencyInjectionContainerHolder(
                 ParticipantGridCellViewModelFactory(),
                 application.resources.getInteger(R.integer.azure_communication_ui_calling_max_remote_participants),
                 container.debugInfoManager,
-                container.configuration.enableMultitasking
+                container.configuration.enableMultitasking,
+                container.configuration.telecomOptions != null
             ),
             container.networkManager,
             callType,
