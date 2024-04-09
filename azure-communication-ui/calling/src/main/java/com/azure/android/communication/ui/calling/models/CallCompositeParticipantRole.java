1dafdab7a7c5407e525e6a6d8efc2815860c732b
Adam Hammer <adamhammer@microsoft.com>
2024-04-09 12:18:55 -0700

    reflagged this file to only use a single flag

--- azure-communication-ui/calling/src/main/java/com/azure/android/communication/ui/calling/models/CallCompositeParticipantRole.java
+++ azure-communication-ui/calling/src/main/java/com/azure/android/communication/ui/calling/models/CallCompositeParticipantRole.java
@@ -1,6 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-
+/* <ROOMS_SUPPORT:0> */
 package com.azure.android.communication.ui.calling.models;
 
 import com.azure.android.core.util.ExpandableStringEnum;
@@ -9,30 +9,22 @@ import java.util.Collection;
 /**
  * Defines values for {@linkCallCompositeParticipantRole}.
  */
-/* <ROOMS_SUPPORT:3> */
 public  final class CallCompositeParticipantRole extends ExpandableStringEnum<CallCompositeParticipantRole> {
-    /* </ROOMS_SUPPORT:0> */
 
     /**
      * Presenter Role in the Room call.
      */
-    /* <ROOMS_SUPPORT:3> */
     public static final CallCompositeParticipantRole PRESENTER = fromString("Presenter");
-    /* </ROOMS_SUPPORT:0> */
 
     /**
      * Attendee Role in the Room call.
      */
-    /* <ROOMS_SUPPORT:3> */
     public static final CallCompositeParticipantRole ATTENDEE = fromString("Attendee");
-    /* </ROOMS_SUPPORT:0> */
 
     /**
      * Creates instance of {@linkCallCompositeParticipantRole}.
      */
-    /* <ROOMS_SUPPORT:3> */
     public CallCompositeParticipantRole() { }
-    /* </ROOMS_SUPPORT:0> */
 
     /**
      * Creates or finds a {@linkCallCompositeParticipantRole} from it's string representation.
@@ -40,16 +32,13 @@ public  final class CallCompositeParticipantRole extends ExpandableStringEnum<Ca
      * @param name a name to look for.
      * @return the corresponding {@linkCallCompositeParticipantRole}.
      */
-    /* <ROOMS_SUPPORT:6> */
     public static CallCompositeParticipantRole fromString(final String name) {
         return fromString(name, CallCompositeParticipantRole.class);
     }
-    /* </ROOMS_SUPPORT:0> */
 
     /**
      * @return known {@linkCallCompositeParticipantRole} values.
      */
-    /* <ROOMS_SUPPORT:3> */
     public static Collection<CallCompositeParticipantRole> values() {
         return values(CallCompositeParticipantRole.class);
     }
