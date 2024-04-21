5c8df98f3b57a40281b3145b0b460eb28f993f59
iaulakh <iaulakh@microsoft.com>
2024-04-21 10:13:19 -0700

    [Calling][Captions][Bug] notify captions type on call state connected

--- azure-communication-ui/calling/src/main/java/com/azure/android/communication/ui/calling/service/sdk/CallingSDKEventHandler.kt
+++ azure-communication-ui/calling/src/main/java/com/azure/android/communication/ui/calling/service/sdk/CallingSDKEventHandler.kt
@@ -402,14 +402,14 @@ internal class CallingSDKEventHandler(
             CallState.CONNECTED -> {
                 addParticipants(call!!.remoteParticipants)
                 onRemoteParticipantUpdated()
-                call?.feature(Features.CAPTIONS)?.addOnActiveCaptionsTypeChangedListener(onCaptionsTypeChanged)
+                subscribeFeatures()
             }
             CallState.NONE, CallState.DISCONNECTED -> {
                 callEndStatus = call?.callEndReason?.let { callEndReason ->
                     Pair(callEndReason.code, callEndReason.subcode)
                 } ?: Pair(0, 0)
                 call?.removeOnStateChangedListener(onCallStateChanged)
-                call?.feature(Features.CAPTIONS)?.removeOnActiveCaptionsTypeChangedListener(onCaptionsTypeChanged)
+                unsubscribeFeatures()
             }
             else -> {}
         }
@@ -423,6 +423,21 @@ internal class CallingSDKEventHandler(
                 }
             }
         }
+    }
+
+    private fun subscribeFeatures() {
+        val captions = call?.feature(Features.CAPTIONS)
+        captions?.addOnActiveCaptionsTypeChangedListener(onCaptionsTypeChanged)
+        captions?.captions?.whenComplete { callCaptions, throwable ->
+            if (throwable == null) {
+                this.callCaptions = callCaptions
+                setCaptionsType(callCaptions)
+            }
+        }
+    }
+
+    private fun unsubscribeFeatures() {
+        call?.feature(Features.CAPTIONS)?.removeOnActiveCaptionsTypeChangedListener(onCaptionsTypeChanged)
     }
 
     private fun onIsMutedChange() {
@@ -687,7 +702,7 @@ internal class CallingSDKEventHandler(
             captionsCallFeature?.captions?.whenComplete { callCaptions, throwable ->
                 if (throwable == null) {
                     this.callCaptions = callCaptions
-                    captionsTypeChanged(callCaptions)
+                    setCaptionsType(callCaptions)
                 }
             }
         }
@@ -731,7 +746,7 @@ internal class CallingSDKEventHandler(
         }
     }
 
-    private fun captionsTypeChanged(captions: CallCaptions) {
+    private fun setCaptionsType(captions: CallCaptions) {
         if (captions is TeamsCaptions) {
             coroutineScope.launch {
                 captionsTypeChangedSharedFlow.emit(CallCompositeCaptionsType.TEAMS)
