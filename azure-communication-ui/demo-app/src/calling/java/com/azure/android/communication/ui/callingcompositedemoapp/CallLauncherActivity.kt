bf99a6694943c79523cb99eb39efea637a9db97b
pavelprystinka <30868871+pavelprystinka@users.noreply.github.com>
2024-06-20 09:45:36 -0700

    [Calling][Bug] Do not need to kill call composite when DemoApp activity is closed (#1028)

--- azure-communication-ui/demo-app/src/calling/java/com/azure/android/communication/ui/callingcompositedemoapp/CallLauncherActivity.kt
+++ azure-communication-ui/demo-app/src/calling/java/com/azure/android/communication/ui/callingcompositedemoapp/CallLauncherActivity.kt
@@ -326,7 +326,6 @@ class CallLauncherActivity : AppCompatActivity() {
         unregisterReceiver(callLauncherBroadCastReceiver)
         DismissCompositeButtonView.get(this).hide()
         DismissCompositeButtonView.buttonView = null
-        (application as CallLauncherApplication).onDestroy()
     }
 
     // check whether new Activity instance was brought to top of stack,
