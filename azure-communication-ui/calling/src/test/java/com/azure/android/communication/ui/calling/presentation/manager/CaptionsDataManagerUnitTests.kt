5a0b04b200c768aee98232b2f02f735ce09b1622
iaulakh <iaulakh@microsoft.com>
2024-07-02 20:41:51 -0700

    Update CaptionsDataManagerUnitTests.kt

--- azure-communication-ui/calling/src/test/java/com/azure/android/communication/ui/calling/presentation/manager/CaptionsDataManagerUnitTests.kt
+++ azure-communication-ui/calling/src/test/java/com/azure/android/communication/ui/calling/presentation/manager/CaptionsDataManagerUnitTests.kt
@@ -13,9 +13,12 @@ import com.azure.android.communication.ui.calling.redux.state.AppReduxState
 import com.azure.android.communication.ui.calling.redux.state.CaptionsState
 import com.azure.android.communication.ui.calling.redux.state.ReduxState
 import com.azure.android.communication.ui.calling.service.CallingService
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.TestScope
@@ -228,5 +231,86 @@ internal class CaptionsDataManagerUnitTests : ACSBaseTestCoroutine() {
             assertNull(newCaption)
             flowJob.cancel()
         }
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun captionsDataManagerUnitTest_when_stateFlowUpdated_multipleUpdates() {
+        runScopedTest {
+            // Arrange
+            val sharedFlow = MutableSharedFlow<CallCompositeCaptionsData>()
+            `when`(callingService.getCaptionsReceivedSharedFlow()).thenReturn(sharedFlow)
+            `when`(appStore.getCurrentState()).thenReturn(AppReduxState(displayName = ""))
+
+            val timestamp = Date()
+            val captionDataAdd1 = createCaptionData("123", "abc", "def", CaptionsResultType.PARTIAL, timestamp)
+            val captionDataUpdate1 = createCaptionData("123", "abc234", "def", CaptionsResultType.FINAL, Date())
+            val captionDataAdd2 = createCaptionData("456", "xyz", "uvw", CaptionsResultType.PARTIAL, Date())
+            val captionDataUpdate2 = createCaptionData("456", "xyz890", "uvw", CaptionsResultType.PARTIAL, Date())
+
+            val addedData = mutableListOf<CaptionsManagerData?>()
+            val updatedData = mutableListOf<CaptionsManagerData?>()
+
+            // Act
+            val addedDataJob = collectStateFlow(captionsDataManager.getOnNewCaptionsDataAddedStateFlow(), addedData)
+            val updatedDataJob = collectStateFlow(captionsDataManager.getOnLastCaptionsDataUpdatedStateFlow(), updatedData)
+
+            captionsDataManager.start(TestScope(UnconfinedTestDispatcher()))
+
+            val jobList = mutableListOf<Job>()
+            val data = listOf(captionDataAdd1, captionDataUpdate1, captionDataAdd2, captionDataUpdate2)
+            data.forEach {
+                val job = launch {
+                    sharedFlow.emit(it)
+                }
+                jobList.add(job)
+                job.join()
+            }
+
+            // Assert
+            assertEquals(3, addedData.size)
+            assertEquals(3, updatedData.size)
+
+            assertNull(addedData[0])
+            assertNull(updatedData[0])
+
+            assertEquals("abc", addedData[1]?.displayText)
+            assertEquals("abc234", updatedData[1]?.displayText)
+
+            assertEquals("xyz", addedData[2]?.displayText)
+            assertEquals("xyz890", updatedData[2]?.displayText)
+
+            addedDataJob.cancel()
+            updatedDataJob.cancel()
+            jobList.forEach {
+                it.cancel()
+            }
+        }
+    }
+
+    private fun createCaptionData(
+        speakerRawId: String,
+        spokenText: String,
+        spokenLanguage: String,
+        resultType: CaptionsResultType,
+        timestamp: Date
+    ): CallCompositeCaptionsData {
+        return CallCompositeCaptionsData(
+            speakerName = "Speaker",
+            captionText = "",
+            speakerRawId = speakerRawId,
+            captionLanguage = "",
+            spokenText = spokenText,
+            spokenLanguage = spokenLanguage,
+            resultType = resultType,
+            timestamp = timestamp
+        )
+    }
+
+    private fun CoroutineScope.collectStateFlow(
+        stateFlow: StateFlow<CaptionsManagerData?>,
+        collector: MutableList<CaptionsManagerData?>
+    ) = launch {
+        stateFlow.toList(collector)
     }
 }
