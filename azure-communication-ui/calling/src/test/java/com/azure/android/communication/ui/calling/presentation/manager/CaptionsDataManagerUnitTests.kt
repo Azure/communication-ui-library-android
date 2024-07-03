c2a4e2227cd78208eb1456548eacfad0842af379
iaulakh <iaulakh@microsoft.com>
2024-07-02 20:09:41 -0700

    Update CaptionsDataManagerUnitTests.kt

--- azure-communication-ui/calling/src/test/java/com/azure/android/communication/ui/calling/presentation/manager/CaptionsDataManagerUnitTests.kt
+++ azure-communication-ui/calling/src/test/java/com/azure/android/communication/ui/calling/presentation/manager/CaptionsDataManagerUnitTests.kt
@@ -6,25 +6,28 @@ package com.azure.android.communication.ui.calling.presentation.manager
 import com.azure.android.communication.ui.calling.ACSBaseTestCoroutine
 import com.azure.android.communication.ui.calling.models.CallCompositeCaptionsData
 import com.azure.android.communication.ui.calling.models.CaptionsResultType
+import com.azure.android.communication.ui.calling.presentation.fragment.calling.captions.CaptionsManagerData
 
 import com.azure.android.communication.ui.calling.redux.AppStore
 import com.azure.android.communication.ui.calling.redux.state.AppReduxState
 import com.azure.android.communication.ui.calling.redux.state.ReduxState
 import com.azure.android.communication.ui.calling.service.CallingService
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertNull
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.Mockito.`when`
 import org.mockito.junit.MockitoJUnitRunner
-import org.mockito.kotlin.times
 import java.util.Date
 
 @RunWith(MockitoJUnitRunner::class)
@@ -118,6 +121,102 @@ internal class CaptionsDataManagerUnitTests : ACSBaseTestCoroutine() {
             assertEquals(timestamp, newCaption?.timestamp)
             assertEquals("def", newCaption?.languageCode)
             assertEquals(false, newCaption?.isFinal)
+            flowJob.cancel()
+        }
+    }
+
+    @OptIn(InternalCoroutinesApi::class)
+    @Test
+    @ExperimentalCoroutinesApi
+    fun captionsDataManagerUnitTest_when_stateFlowUpdated_addAndUpdateNotified() {
+        runScopedTest {
+            // Arrange
+            val sharedFlow = MutableSharedFlow<CallCompositeCaptionsData>()
+            `when`(callingService.getCaptionsReceivedSharedFlow()).thenReturn(sharedFlow)
+            `when`(appStore.getCurrentState()).thenReturn(AppReduxState(displayName = ""))
+            val timestamp = Date()
+            val captionDataAdd = CallCompositeCaptionsData(
+                speakerName = "Speaker",
+                captionText = "",
+                speakerRawId = "123",
+                captionLanguage = "",
+                spokenText = "abc",
+                spokenLanguage = "def",
+                resultType = CaptionsResultType.PARTIAL,
+                timestamp = timestamp
+            )
+            val captionDataUpdate = CallCompositeCaptionsData(
+                speakerName = "Speaker",
+                captionText = "",
+                speakerRawId = "123",
+                captionLanguage = "",
+                spokenText = "abc234",
+                spokenLanguage = "def",
+                resultType = CaptionsResultType.PARTIAL,
+                timestamp = Date()
+            )
+            val addedData = mutableListOf<CaptionsManagerData?>()
+            val updatedData = mutableListOf<CaptionsManagerData?>()
+
+            // Act
+            val addedDataJob = launch {
+                captionsDataManager.getOnNewCaptionsDataAddedStateFlow().toList(addedData)
+            }
+            val updatedDataJob = launch {
+                captionsDataManager.getOnLastCaptionsDataUpdatedStateFlow().toList(updatedData)
+            }
+            captionsDataManager.start(TestScope(UnconfinedTestDispatcher()))
+            val flowJob = launch {
+                sharedFlow.emit(captionDataAdd)
+                sharedFlow.emit(captionDataUpdate)
+            }
+            flowJob.join()
+
+            // Assert
+            assertEquals(2, addedData.size)
+            assertEquals(2, updatedData.size)
+            assertNull(addedData[0])
+            assertNull(updatedData[0])
+            assertEquals("abc", addedData[1]?.displayText)
+            assertEquals("abc234", updatedData[1]?.displayText)
+
+            flowJob.cancel()
+            addedDataJob.cancel()
+            updatedDataJob.cancel()
+        }
+    }
+
+    @Test
+    @ExperimentalCoroutinesApi
+    fun captionsDataManagerUnitTest_when_captionLanguageIsActive_doesNotNotifyIfCaptionTextIsBlank() {
+        runScopedTest {
+            // Arrange
+            val sharedFlow = MutableSharedFlow<CallCompositeCaptionsData>()
+            val currentState = AppReduxState(displayName = "")
+            `when`(callingService.getCaptionsReceivedSharedFlow()).thenReturn(sharedFlow)
+            `when`(appStore.getCurrentState()).thenReturn(currentState)
+            val timestamp = Date()
+            val captionData = CallCompositeCaptionsData(
+                speakerName = "Speaker",
+                captionText = "",
+                speakerRawId = "123",
+                captionLanguage = "",
+                spokenText = "abc",
+                spokenLanguage = "def",
+                resultType = CaptionsResultType.PARTIAL,
+                timestamp = timestamp
+            )
+
+            // Act
+            captionsDataManager.start(TestScope(UnconfinedTestDispatcher()))
+            val flowJob = launch {
+                sharedFlow.emit(captionData)
+            }
+            flowJob.join()
+
+            // Assert
+            val newCaption = captionsDataManager.getOnNewCaptionsDataAddedStateFlow().value
+            assertNull(newCaption)
             flowJob.cancel()
         }
     }
