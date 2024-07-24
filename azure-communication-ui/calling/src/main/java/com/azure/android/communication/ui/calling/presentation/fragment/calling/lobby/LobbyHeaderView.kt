5883fa7706958371c44e95fdacb1ede8ee581407
Mohtasim <mbellah@microsoft.com>
2024-07-24 16:36:25 -0700

    Ktlint Format changes
    Signed-off-by: Mohtasim <mbellah@microsoft.com>

--- azure-communication-ui/calling/src/main/java/com/azure/android/communication/ui/calling/presentation/fragment/calling/lobby/LobbyHeaderView.kt
+++ azure-communication-ui/calling/src/main/java/com/azure/android/communication/ui/calling/presentation/fragment/calling/lobby/LobbyHeaderView.kt
@@ -8,11 +8,9 @@ import android.util.AttributeSet
 import android.view.View
 import android.widget.ImageButton
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.view.ViewCompat
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
 import com.azure.android.communication.ui.calling.implementation.R
-import com.azure.android.communication.ui.calling.utilities.isAndroidTV
 import com.microsoft.fluentui.widget.Button
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
