90fb41307a7f7c968b3ee8dc173de797e6c53111
Pavel Prystinka <pprystinka@microsoft.com>
2024-10-09 12:08:50 -0700

    Add call screen header custom button test

--- azure-communication-ui/calling/src/test/java/com/azure/android/communication/ui/calling/presentation/fragment/calling/header/InfoHeaderViewModelUnitTest.kt
+++ azure-communication-ui/calling/src/test/java/com/azure/android/communication/ui/calling/presentation/fragment/calling/header/InfoHeaderViewModelUnitTest.kt
@@ -6,9 +6,11 @@ package com.azure.android.communication.ui.calling.presentation.fragment.calling
 import com.azure.android.communication.ui.calling.ACSBaseTestCoroutine
 import com.azure.android.communication.ui.calling.models.ParticipantInfoModel
 import com.azure.android.communication.ui.calling.redux.state.AppReduxState
+import com.azure.android.communication.ui.calling.redux.state.ButtonState
 import com.azure.android.communication.ui.calling.redux.state.CallScreenInfoHeaderState
 import com.azure.android.communication.ui.calling.redux.state.CallingState
 import com.azure.android.communication.ui.calling.redux.state.CallingStatus
+import com.azure.android.communication.ui.calling.redux.state.CustomButtonState
 import com.azure.android.communication.ui.calling.redux.state.RemoteParticipantsState
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.toList
@@ -271,4 +273,121 @@ internal class InfoHeaderViewModelUnitTest : ACSBaseTestCoroutine() {
         }
     }
     /* </CUSTOM_CALL_HEADER> */
+
+    /* <CALL_SCREEN_HEADER_CUSTOM_BUTTONS:0> */
+    @ExperimentalCoroutinesApi
+    @Test
+    fun floatingHeaderViewModel_update_then_displayCustomButtons() {
+        runScopedTest {
+            val appState = AppReduxState("", false, false)
+
+            val expectedParticipantMap: Map<String, ParticipantInfoModel> = mapOf()
+            val timestamp: Number = System.currentTimeMillis()
+
+            appState.remoteParticipantState = RemoteParticipantsState(
+                expectedParticipantMap,
+                timestamp,
+                listOf(),
+                0,
+                lobbyErrorCode = null,
+                totalParticipantCount = 0,
+            )
+            appState.callState = CallingState(
+                CallingStatus.CONNECTED,
+                joinCallIsRequested = false,
+                isRecording = false,
+                isTranscribing = false
+            )
+            appState.buttonState = ButtonState(callScreenHeaderCustomButtonsState = listOf())
+            val title = "title"
+            val subtitle = "subtitle"
+
+            val floatingHeaderViewModel = InfoHeaderViewModel(
+                false,
+                /* <CALL_SCREEN_HEADER_CUSTOM_BUTTONS:0> */
+                mock(), mock()
+                /* </CALL_SCREEN_HEADER_CUSTOM_BUTTONS> */
+            )
+            floatingHeaderViewModel.init(
+                appState.callState.callingStatus,
+                expectedParticipantMap.count(),
+                CallScreenInfoHeaderState(title, subtitle),
+                /* <CALL_SCREEN_HEADER_CUSTOM_BUTTONS:0> */
+                appState.buttonState,
+                /* </CALL_SCREEN_HEADER_CUSTOM_BUTTONS> */
+            ) { }
+
+            val customButton1StateFlow =
+                mutableListOf<InfoHeaderViewModel.CustomButtonEntry?>()
+            val customButton2StateFlow =
+                mutableListOf<InfoHeaderViewModel.CustomButtonEntry?>()
+
+            val flowButton1 = launch {
+                floatingHeaderViewModel.getCustomButton1StateFlow()
+                    .toList(customButton1StateFlow)
+            }
+            val flowButton2 = launch {
+                floatingHeaderViewModel.getCustomButton2StateFlow()
+                    .toList(customButton2StateFlow)
+            }
+
+            // act
+            val button1 = CustomButtonState(
+                id = "id1",
+                isEnabled = true,
+                isVisible = true,
+                title = "title1",
+                drawableId = 1
+            )
+            val button2 = CustomButtonState(
+                id = "id2",
+                isEnabled = true,
+                isVisible = true,
+                title = "title2",
+                drawableId = 2
+            )
+
+            val buttonState1 = ButtonState(callScreenHeaderCustomButtonsState = listOf(button1))
+            floatingHeaderViewModel.update(
+                expectedParticipantMap.count(),
+                CallScreenInfoHeaderState(null, null),
+                /* <CALL_SCREEN_HEADER_CUSTOM_BUTTONS:0> */
+                buttonState1,
+                /* </CALL_SCREEN_HEADER_CUSTOM_BUTTONS> */
+            )
+
+            Assert.assertEquals(2, customButton1StateFlow.size)
+            Assert.assertEquals(1, customButton2StateFlow.size)
+
+            Assert.assertEquals(button1.id, customButton1StateFlow[1]?.id)
+            Assert.assertEquals(button1.title, customButton1StateFlow[1]?.titleText)
+            Assert.assertEquals(button1.drawableId, customButton1StateFlow[1]?.icon)
+            Assert.assertEquals(button1.isEnabled, customButton1StateFlow[1]?.isEnabled)
+            Assert.assertEquals(button1.isVisible, customButton1StateFlow[1]?.isVisible)
+
+            val buttonState2 = ButtonState(callScreenHeaderCustomButtonsState = listOf(button1, button2))
+
+            // act
+            floatingHeaderViewModel.update(
+                expectedParticipantMap.count(),
+                CallScreenInfoHeaderState(null, null),
+                /* <CALL_SCREEN_HEADER_CUSTOM_BUTTONS:0> */
+                buttonState2,
+                /* </CALL_SCREEN_HEADER_CUSTOM_BUTTONS> */
+            )
+
+            Assert.assertEquals(2, customButton1StateFlow.size)
+            Assert.assertEquals(2, customButton2StateFlow.size)
+
+            Assert.assertEquals(button2.id, customButton2StateFlow[1]?.id)
+            Assert.assertEquals(button2.title, customButton2StateFlow[1]?.titleText)
+            Assert.assertEquals(button2.drawableId, customButton2StateFlow[1]?.icon)
+            Assert.assertEquals(button2.isEnabled, customButton2StateFlow[1]?.isEnabled)
+            Assert.assertEquals(button2.isVisible, customButton2StateFlow[1]?.isVisible)
+
+            flowButton1.cancel()
+            flowButton2.cancel()
+        }
+    }
+    /* </CALL_SCREEN_HEADER_CUSTOM_BUTTONS:0> */
 }
