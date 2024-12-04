ddd499096e79c5ed60546100a5025fcc704c89d5
ShaunaSong <93549644+ShaunaSong@users.noreply.github.com>
2024-12-03 16:48:52 -0800

    [Calling][Release] Disable the beta feature (#1118)

--- azure-communication-ui/calling/src/main/java/com/azure/android/communication/ui/calling/CallComposite.java
+++ azure-communication-ui/calling/src/main/java/com/azure/android/communication/ui/calling/CallComposite.java
@@ -57,9 +57,9 @@ import com.jakewharton.threetenabp.AndroidThreeTen;
 import java.util.Collection;
 import java.util.Collections;
 
-/*  <CALL_START_TIME> */
+/*  <CALL_START_TIME>
 import java.util.Date;
-/* </CALL_START_TIME> */
+</CALL_START_TIME> */
 import java.util.List;
 /* <RTT_POC>
 import java.util.Timer;
@@ -625,31 +625,31 @@ public final class CallComposite {
      *
      * @param eventHandler The {@link CallCompositeEventHandler}.
      */
-    /* <CALL_START_TIME> */
+    /* <CALL_START_TIME>
     public void addOnCallStartTimeUpdatedEventHandler(
             final CallCompositeEventHandler<Date> eventHandler) {
         configuration.getCallCompositeEventsHandler().addOnCallStartTimeUpdatedEventHandler(eventHandler);
     }
-    /* </CALL_START_TIME> */
+    </CALL_START_TIME> */
 
     /**
      * Remove {@link CallCompositeEventHandler}.
      *
      * @param eventHandler The {@link CallCompositeEventHandler}.
      */
-    /* <CALL_START_TIME> */
+    /* <CALL_START_TIME>
     public void removeOnCallStartTimeUpdatedEventHandler(
             final CallCompositeEventHandler<Date> eventHandler) {
         configuration.getCallCompositeEventsHandler().removeOnCallStartTimeUpdatedEventHandler(eventHandler);
     }
-    /* </CALL_START_TIME> */
+    </CALL_START_TIME> */
 
     /**
      * Get Call Start Time.
      *
      * @return {@link Date}
      */
-    /* <CALL_START_TIME> */
+    /* <CALL_START_TIME>
     public Date getCallStartTime() {
         final DependencyInjectionContainer container = diContainer;
         if (container != null) {
@@ -657,7 +657,7 @@ public final class CallComposite {
         }
         return null;
     }
-    /* </CALL_START_TIME> */
+    </CALL_START_TIME> */
 
     /**
      * Add {@link CallCompositeEventHandler}
