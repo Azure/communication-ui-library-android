2c127c3311aca7858e32922aad3656f3edf91525
iaulakh <iaulakh@microsoft.com>
2025-01-02 16:14:27 -0800

    [Calling][Feature] display call duration on UI

--- azure-communication-ui/demo-app/src/calling/java/com/azure/android/communication/ui/callingcompositedemoapp/CallCompositeManager.kt
+++ azure-communication-ui/demo-app/src/calling/java/com/azure/android/communication/ui/callingcompositedemoapp/CallCompositeManager.kt
@@ -719,6 +719,9 @@ class CallCompositeManager(private val context: Context) {
         }
         if (!SettingsFeatures.getCallScreenInformationTitle().isNullOrEmpty() ||
             !SettingsFeatures.getCallScreenInformationSubtitle().isNullOrEmpty() ||
+            /* <CALL_START_TIME> */
+            SettingsFeatures.getCallScreenShowCallDuration() != null ||
+            /* </CALL_START_TIME> */
             SettingsFeatures.getCallScreenInformationTitleUpdateParticipantCount() != 0 ||
             SettingsFeatures.getCallScreenInformationSubtitleUpdateParticipantCount() != 0
         ) {
@@ -736,6 +739,11 @@ class CallCompositeManager(private val context: Context) {
                     callScreenHeaderOptions?.subtitle = it
                 }
             }
+            /* <CALL_START_TIME> */
+            SettingsFeatures.getCallScreenShowCallDuration()?.let {
+                callScreenHeaderOptions?.showCallDuration = it
+            }
+            /* </CALL_START_TIME> */
         }
         if (SettingsFeatures.getAddCustomButtons() == true) {
             val headerButton1 =
