62216f5d82f5b4e13c64912f6dd101afd34ab8db
ShaunaSong <93549644+ShaunaSong@users.noreply.github.com>
2025-01-29 09:21:16 -0800

    [Calling][Bug] Update the bottom sheet accessibility  (#1084)

--- azure-communication-ui/calling/src/main/java/com/azure/android/communication/ui/calling/presentation/fragment/calling/controlbar/captions/CaptionsLanguageSelectionListView.kt
+++ azure-communication-ui/calling/src/main/java/com/azure/android/communication/ui/calling/presentation/fragment/calling/controlbar/captions/CaptionsLanguageSelectionListView.kt
@@ -99,10 +99,18 @@ internal class CaptionsLanguageSelectionListView(
 
     private fun getBottomCellItems(): List<BottomCellItem> {
         val items = mutableListOf<BottomCellItem>()
-        viewModel.languagesListStateFlow.value.forEach { language ->
+        viewModel.languagesListStateFlow.value.forEachIndexed { index, language ->
+            val contentDescription = context.getString(
+                R.string.azure_communication_ui_calling_list_item,
+                getLocaleDisplayName(language),
+                (index + 1),
+                viewModel.languagesListStateFlow.value.size,
+            )
+
             items.add(
                 BottomCellItem(
                     title = getLocaleDisplayName(language),
+                    contentDescription = contentDescription,
                     isChecked = language == viewModel.updateActiveLanguageStateFlow.value,
                     itemType = BottomCellItemType.BottomMenuActionNoIcon,
                     onClickAction = {
